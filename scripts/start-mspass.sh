65a734d69bebd23345c6a3e919ead55f00f874d0
Ian Wang <yinzhi.wang.cug@gmail.com>
2019-08-29 16:32:20 -0500

    enabled mongo access from worker container

--- scripts/start-mspass.sh
+++ scripts/start-mspass.sh
@@ -2,7 +2,7 @@
 
 if [ "$MSPASS_ROLE" = "master" ]; then
   $SPARK_HOME/sbin/start-master.sh
-  mongod --dbpath $MONGO_DATA --logpath $MONGO_LOG
+  mongod --dbpath $MONGO_DATA --logpath $MONGO_LOG --bind_ip_all
 fi
 if [ "$MSPASS_ROLE" = "worker" ]; then
   $SPARK_HOME/sbin/start-slave.sh spark://$SPARK_MASTER:$SPARK_MASTER_PORT
