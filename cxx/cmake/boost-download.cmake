4738ea3ec9cef54394dc38ac32e4d293be3909ba
wangyinz <yinzhi.wang.cug@gmail.com>
2019-09-12 19:05:04 -0500

    fixed syntax error

--- cxx/cmake/boost-download.cmake
+++ cxx/cmake/boost-download.cmake
@@ -7,14 +7,13 @@ include(ExternalProject)
 ExternalProject_Add(
   boost
   SOURCE_DIR "@BOOST_DOWNLOAD_ROOT@/boost-src"
-  BINARY_DIR "@BOOST_DOWNLOAD_ROOT@/boost-build"
   URL
-    https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2
+    https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.gz
   URL_HASH
-    SHA256=d73a8da01e8bf8c7eda40b4c84915071a8c8a0df4a6734537ddde4a8580524ee
-  CONFIGURE_COMMAND "./bootstrap.sh --prefix=${PROJECT_BINARY_DIR}/boost"
-  BUILD_COMMAND "./b2"
+    SHA256=96b34f7468f26a141f6020efb813f1a2f3dfb9797ecf76a7d7cbd843cc95f5bd
+  CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR}
+  BUILD_COMMAND ./b2
   BUILD_IN_SOURCE 1
-  INSTALL_COMMAND "./b2 install"
+  INSTALL_COMMAND ./b2 install
   TEST_COMMAND ""
   )
--- cxx/cmake/openblas-download.cmake
+++ cxx/cmake/openblas-download.cmake
@@ -7,15 +7,14 @@ include(ExternalProject)
 ExternalProject_Add(
   openblas
   SOURCE_DIR "@OPENBLAS_DOWNLOAD_ROOT@/openblas-src"
-  BINARY_DIR "@OPENBLAS_DOWNLOAD_ROOT@/openblas-build"
   GIT_REPOSITORY
     https://github.com/xianyi/OpenBLAS.git 
   GIT_TAG
     v0.3.7
-  UPDATE_COMMAND "echo update"
-  CONFIGURE_COMMAND "echo config"
-  BUILD_COMMAND "${CMAKE_BUILD_TOOL}"
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ${CMAKE_BUILD_TOOL}
   BUILD_IN_SOURCE 1
-  INSTALL_COMMAND "${CMAKE_BUILD_TOOL} install PREFIX=${PROJECT_BINARY_DIR}/openblas"
+  INSTALL_COMMAND ${CMAKE_BUILD_TOOL} install PREFIX=${PROJECT_BINARY_DIR}
   TEST_COMMAND ""
   )
