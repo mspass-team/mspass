efe4eaa4d7178859e2707b1ddfe98a502d14b6f3
Ian Wang <yinzhi.wang.cug@gmail.com>
2019-09-23 17:22:50 -0500

    save both the waveform and the metadata

--- python/example.py
+++ python/example.py
@@ -7,11 +7,20 @@ client = Client("IRIS")
 
 from obspy import UTCDateTime
 t = UTCDateTime("2010-02-27T06:45:00.000")
-
 st = client.get_waveforms("IU", "ANMO", "00", "BH*", t, t + 60 * 60)
-data = spark.sparkContext.parallelize(st)
-metadata = data.map(lambda tr: tr.stats)
-dfMeta = metadata.toDF()
-dfMeta.write.format("mongo").mode("append").option("database",
+
+#from pyspark.ml.linalg import Vectors
+#data = spark.sparkContext.parallelize(st).map(lambda tr: (tr.stats, Vectors.dense(tr.data)))
+data = spark.sparkContext.parallelize(st).map(lambda tr: (tr.stats, tr.data.tolist()))
+df = data.toDF(["metadata", "waveform"])
+
+from pyspark.sql.functions import monotonically_increasing_id
+dfmetadata = df.select("metadata.*").withColumn("tmpid", monotonically_increasing_id())
+dfdata = df.select("waveform").withColumn("tmpid", monotonically_increasing_id())
+df = dfmetadata.join(dfdata, "tmpid", "outer").drop("tmpid")
+
+df.show()
+
+df.write.format("mongo").mode("append").option("database",
 "test").option("collection", "wfdisc").save()
 
