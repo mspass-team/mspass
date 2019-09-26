08b1fb105a8bf1dc286efc453598f8756e4efb2b
Ian Wang <yinzhi.wang.cug@gmail.com>
2019-09-26 15:20:16 -0500

    waveform now serialized into gridfs

--- python/example.py
+++ python/example.py
@@ -9,18 +9,27 @@ from obspy import UTCDateTime
 t = UTCDateTime("2010-02-27T06:45:00.000")
 st = client.get_waveforms("IU", "ANMO", "00", "BH*", t, t + 60 * 60)
 
-#from pyspark.ml.linalg import Vectors
-#data = spark.sparkContext.parallelize(st).map(lambda tr: (tr.stats, Vectors.dense(tr.data)))
+# Convert the downloaded ensemble to an RDD
 data = spark.sparkContext.parallelize(st).map(lambda tr: (tr.stats, tr.data.tolist()))
+
+# Convert RDD to dataframe with 2 columns
 df = data.toDF(["metadata", "waveform"])
 
+# Flatten the metadata struct and add a tmpid for later join
 from pyspark.sql.functions import monotonically_increasing_id
 dfmetadata = df.select("metadata.*").withColumn("tmpid", monotonically_increasing_id())
 dfdata = df.select("waveform").withColumn("tmpid", monotonically_increasing_id())
+
+# Save the waveform to gridfs and replace the column with the record's id
+from pymongo import MongoClient
+import gridfs
+import pickle
+dfdata = dfdata.rdd.map(lambda x: ([str(gridfs.GridFS(MongoClient('mongodb://mspass-master:27017/').gridfs_wf).put(pickle.dumps(x["waveform"])))], x["tmpid"])).cache().toDF(["wfid", "tmpid"])
+
+# Join metadata with the wfid of saved data
 df = dfmetadata.join(dfdata, "tmpid", "outer").drop("tmpid")
 
 df.show()
 
-df.write.format("mongo").mode("append").option("database",
-"test").option("collection", "wfdisc").save()
+df.write.format("mongo").mode("append").option("database","test").option("collection", "wfdisc").save()
 
