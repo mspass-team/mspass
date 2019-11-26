c6123c9f56fb497a000b3022bdd1ea1e4755289e
Ian Wang <yinzhi.wang.cug@gmail.com>
2019-11-26 17:28:55 -0600

    cmake automatically builds pybind11 when not found

---
+++ cxx/cmake/pybind11-download.cmake
@@ -0,0 +1,20 @@
+cmake_minimum_required(VERSION 3.4)
+
+project(pybind11-download NONE)
+
+include(ExternalProject)
+
+ExternalProject_Add(
+  pybind11
+  SOURCE_DIR "@PYBIND11_DOWNLOAD_ROOT@/pybind11-src"
+  GIT_REPOSITORY
+    https://github.com/pybind/pybind11.git
+  GIT_TAG
+    v2.4.3
+  UPDATE_COMMAND bash -c "pip show pytest || pip install --user pytest"
+  CONFIGURE_COMMAND cmake -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR} .
+  BUILD_COMMAND make -j 8
+  BUILD_IN_SOURCE 1
+  INSTALL_COMMAND make install 
+  TEST_COMMAND ""
+  )
