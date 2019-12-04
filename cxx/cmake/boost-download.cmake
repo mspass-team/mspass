f0f1d21cf2e2c18a7e9d443ad00fac079e027ce0
Ian Wang <22849821+wangyinz@users.noreply.github.com>
2019-12-04 09:54:42 -0600

    Wangyinz travis patch 1 (#25)
    * Patch python.jam to use abiflags
    
    This is a temporary fix based on the discussion from https://github.com/boostorg/build/pull/250
    
    * add boost include path to python component

--- cxx/cmake/boost-download.cmake
+++ cxx/cmake/boost-download.cmake
@@ -11,6 +11,7 @@ ExternalProject_Add(
     https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.gz
   URL_HASH
     SHA256=96b34f7468f26a141f6020efb813f1a2f3dfb9797ecf76a7d7cbd843cc95f5bd
+  UPDATE_COMMAND wget https://svn.boost.org/trac10/raw-attachment/ticket/11120/python_jam.patch && patch tools/build/src/tools/python.jam python_jam.patch
   CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR}
   BUILD_COMMAND ./b2 -j 8
   BUILD_IN_SOURCE 1
