4180676b1a467252ea915d3fb102df9272c0b333
Ian Wang <22849821+wangyinz@users.noreply.github.com>
2020-01-20 22:19:23 -0600

    Change to create a namespace package
    according to https://packaging.python.org/guides/packaging-namespace-packages/#creating-a-namespace-package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 import platform
 import subprocess
 
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension, find_namespace_packages
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
@@ -68,6 +68,6 @@ setup(
     ext_modules=[CMakeExtension('mspass')],
     cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['mspass.*']),
     install_requires=['pyyaml']
 )
