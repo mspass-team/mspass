255458bdba7ab152ca6bddb879570d1743fb36a9
wangyinz <yinzhi.wang.cug@gmail.com>
2020-02-07 10:06:25 -0600

    changed theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,7 @@
 
 import os
 import subprocess
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -33,6 +34,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+  'sphinx_rtd_theme',
   'sphinx.ext.githubpages',
   'sphinx.ext.autodoc',
   'm2r',
@@ -54,7 +56,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
