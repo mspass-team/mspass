2b70be7b940da04650a8d9c2db7adfb6e6bf3246
Gary Pavlis <pavlis@indiana.edu>
2020-02-08 09:09:45 -0500

    add verbose options and fix documentation to look right with sphynx

--- python/mspasspy/importer/seispp.py
+++ python/mspasspy/importer/seispp.py
@@ -5,8 +5,10 @@ from pymongo import MongoClient
 from mspasspy.ccore import MetadataDefinitions
 from mspasspy.ccore import MDtype
 import numpy as np
-def dbsave_indexdata(filebase,dbname,ext='d3C'):
+def dbsave_indexdata(filebase,dbname,ext='d3C',verbose=False):
     """
+    Import function for data from antelope export_to_mspass.
+
     This function is an import function for Seismogram objects created 
     by the antelope program export_to_mspass.  That program writes 
     header data as a yaml file and the sample data as a raw binary 
@@ -19,20 +21,20 @@ def dbsave_indexdata(filebase,dbname,ext='d3C'):
     of a database.   Readers that want to read this raw data will 
     need to use dir, dfile, and foff to find the right file and read point.
 
-    Args:
-      filename is the base name of the dataset to be read and indexed.
+    :param filename: is the base name of the dataset to be read and indexed.
         The function will look for filename.yaml for the header data and
         filename.ext (Arg 3 defaulting to d3C).  
-      dbname is the database name MongoDB will use to write the data 
+    :param dbname: is the database name MongoDB will use to write the data 
         read.
-      ext is the file extension for the sample data (default is 'd3C').
+    :param ext: is the file extension for the sample data (default is 'd3C').
     """
     # This loads default mspass schema 
     mdef=MetadataDefinitions()
     yamlfile=filebase+'.yaml'
     fh=open(yamlfile)
     d=yaml.load(fh,Loader=yaml.FullLoader)
-    print('Read data for ',len(d),' objects')
+    if(verbose):
+        print('Read data for ',len(d),' objects')
     fh.close()
     # Set up to add to wf collection
     # This is not general, but works of this test with mongo running under docker
@@ -44,17 +46,19 @@ def dbsave_indexdata(filebase,dbname,ext='d3C'):
     # This is needed by the numpy reader 
     dtyp=np.dtype('f8')
     dir=os.path.dirname(os.path.realpath(dfile))
-    print('Setting dir =',dir,' and dfile=',dfile,' for all input')
-    print('Make sure this file exists before trying to read these data')
-    print('This program only builds the wf collection in the database')
-    print('Readers of the raw data will access the sample data from the dir+dfile path')
+    if(verbose):
+        print('Setting dir =',dir,' and dfile=',dfile,' for all input')
+        print('Make sure this file exists before trying to read these data')
+        print('This program only builds the wf collection in the database')
+        print('Readers of the raw data will access the sample data from the dir+dfile path')
     for i in range(len(d)):
         pyd={}   # this is essentially a required python declaration 
         # Since the source is assumed an antelope css3.0 database we
         # assume these will be defined.   Relating them back to the original
         # source would be impossible without these in css3.0 so shouldn't be 
         # an issue
-        print('Working on sta=',d[i]['sta'],' and evid=',d[i]['evid'])
+        if(verbose):
+            print('Working on sta=',d[i]['sta'],' and evid=',d[i]['evid'])
         keys=d[i].keys()
         for k in keys:
           try:
@@ -68,10 +72,13 @@ def dbsave_indexdata(filebase,dbname,ext='d3C'):
             elif(type==MDtype.Boolean):
               pyd[k]=bool(d[i][k])
             else:
-              print("undefined type for key=",k)
+              # These are not optional - always print these if 
+              # this happens to warn user
+              print("Warning(dbsave_indexdata):  undefined type for key=",k)
               print("attribute will not be copied to database")
           except RuntimeError: 
-              print("key =",k," is undefined - skipped")
+              # as above always print this as a warning
+              print("Warning(dbsave_indexdata): key =",k," is undefined - skipped")
 
         pyd['dir']=dir
         pyd['dfile']=dfile
@@ -81,6 +88,6 @@ def dbsave_indexdata(filebase,dbname,ext='d3C'):
         pyd['foff']=foff
         wfid=collection.insert_one(pyd).inserted_id
         junk=np.fromfile(fh,dtype=dtyp,count=ns3c)
-        print('Inserted to wf with Objectid=',wfid)
-    print("Finished with file=",dfile)
-    print("Size of wf collection is now ",collection.count()," documents")
+    if(verbose)
+        print("Finished with file=",dfile)
+        print("Size of wf collection is now ",collection.count()," documents")
