afb24578973456e0b4c7744ab0e1a16a05357d37
Ian Wang <yinzhi.wang.cug@gmail.com>
2020-02-17 16:11:59 -0600

    staticly link boost when built by mspass

--- cxx/cmake/boost-download.cmake
+++ cxx/cmake/boost-download.cmake
@@ -13,7 +13,7 @@ ExternalProject_Add(
     SHA256=96b34f7468f26a141f6020efb813f1a2f3dfb9797ecf76a7d7cbd843cc95f5bd
   UPDATE_COMMAND wget https://svn.boost.org/trac10/raw-attachment/ticket/11120/python_jam.patch && patch tools/build/src/tools/python.jam python_jam.patch
   CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR}
-  BUILD_COMMAND ./b2 -j 8
+  BUILD_COMMAND ./b2 cxxflags=-fPIC --with-serialization -j 8
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND ./b2 install
   TEST_COMMAND ""
