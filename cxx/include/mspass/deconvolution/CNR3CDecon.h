28ec044f53124a4f9e8a7c5b45deb2a8de84a55f
Gary Pavlis <pavlis@indiana.edu>
2020-03-12 17:09:39 -0400

    Move abstract base to CNR3CDecon to get pybind11 to work

--- cxx/include/mspass/deconvolution/CNR3CDecon.h
+++ cxx/include/mspass/deconvolution/CNR3CDecon.h
@@ -2,15 +2,50 @@
 #define __CNR3C_DECON_H__
 #include <vector>
 #include "mspass/utility/AntelopePf.h"
-#include "mspass/deconvolution/Base3CDecon.h"
 #include "mspass/deconvolution/FFTDeconOperator.h"
 #include "mspass/deconvolution/ShapingWavelet.h"
-#include "MTPowerSpectrumEngine.h"
+#include "mspass/deconvolution/MTPowerSpectrumEngine.h"
 #include "mspass/deconvolution/PowerSpectrum.h"
 #include "mspass/seismic/Taper.h"
 #include "mspass/seismic/TimeSeries.h"
 #include "mspass/seismic/Seismogram.h"
 namespace mspass{
+/*! \brief Absract base class for algorithms handling full 3C data.
+*/
+class Base3CDecon
+{
+public:
+    virtual ~Base3CDecon() {};
+    virtual void change_parameters(const mspass::BasicMetadata &md)=0;
+    virtual void loaddata(mspass::Seismogram& d,const int comp)=0;
+    virtual void loadwavelet(const mspass::TimeSeries& w)=0;
+    /* \brief Return the ideal output of the deconvolution operator.
+
+    All deconvolution operators have a implicit or explicit ideal output
+    signal. e.g. for a spiking Wiener filter it is a delta function with or
+    without a lag.  For a shaping wavelt it is the time domain version of the
+    wavelet. */
+    virtual mspass::Seismogram process()=0;
+    /*! \brif Return the actual output of the deconvolution operator.
+
+    The actual output is defined as w^-1*w and is compable to resolution
+    kernels in linear inverse theory.   Although not required we would
+    normally expect this function to be peaked at 0.   Offsets from 0
+    would imply a bias. */
+    virtual mspass::TimeSeries actual_output()=0;
+
+    /*! \brief Return a FIR represention of the inverse filter.
+
+    After any deconvolution is computed one can sometimes produce a finite
+    impulse response (FIR) respresentation of the inverse filter.  */
+    //virtual mspass::TimeSeries inverse_wavelet() = 0;
+    virtual mspass::TimeSeries inverse_wavelet(double) = 0;
+    /*! \brief Return appropriate quality measures.
+
+    Each operator commonly has different was to measure the quality of the
+    result.  This method should return these in a generic Metadata object. */
+    virtual mspass::Metadata QCMetrics()=0;
+};
 /*! \brief Colored Noise Regularizd 3C Deconvolution opertor.
 
 This algorithm is somewhat of a synthesis of the best features from the
@@ -21,18 +56,19 @@ estimation.  Key features are:
 3.  The inverse is regularized by a scaled 3C noise estimate.
 
 This object is what might be called a processing object.  That is, it
-does not use the model of construction creating the thing it defines 
+does not use the model of construction creating the thing it defines
 (Seismogram, for example, uses that paradigm).  Instead the constructor
-defines the operator.  Data are processed by first calling one of the 
+defines the operator.  Data are processed by first calling one of the
 loaddata and loadnoise methods.  Once valid data is loaded call the process
-method to deconvolve the loaded data. Note this approach assumes the 
+method to deconvolve the loaded data. Note this approach assumes the
 data, wavelet, and noise used for the processing are internally consistent.
 The user is warned there a no safeties to validate any consistency because
-it would be hard to do so without causing more problems that it would 
+it would be hard to do so without causing more problems that it would
 solve.  In MsPASS we expect to hide this a bit behind some python wrappers
-to create some safeties.  
+to create some safeties.
 */
-class CNR3CDecon: public FFTDeconOperator, public Base3CDecon
+//class CNR3CDecon : public mspass::FFTDeconOperator, public mspass::Base3CDecon
+class CNR3CDecon : public mspass::Base3CDecon, public mspass::FFTDeconOperator
 {
 public:
   /*! Default constructor.  Puts operator in an invalid state.*/
@@ -40,7 +76,7 @@ public:
   CNR3CDecon(const mspass::AntelopePf& pf);
   CNR3CDecon(const CNR3CDecon& parent);
   ~CNR3CDecon();
-  void change_parameters(const mspass::AntelopePf& md);
+  void change_parameters(const mspass::BasicMetadata& md);
   /*! \brief Load data with one component used as wavelet estimate.
 
   Use this method for conventional receiver function data if one of
@@ -51,10 +87,11 @@ public:
   void loaddata(mspass::Seismogram& d, const mspass::TimeSeries& w);
   void loadnoise(mspass::Seismogram& n);
   void loadnoise(const mspass::PowerSpectrum& n);
+  void loadwavelet(const mspass::TimeSeries& w);
   /* These same names are used in ScalarDecon but we don't inherit them
   here because this algorithm is 3C data centric there is a collision
   with the ScalarDecon api because of it.  */
-  Seismogram process();
+  mspass::Seismogram process();
 
   /* \brief Return the ideal output of the deconvolution operator.
 
@@ -101,7 +138,9 @@ private:
   mspass::Seismogram noisedata;
   mspass::Seismogram decondata;
   mspass::TimeSeries wavelet;
-  mspass::BasicTaper *taper;
+  /* AS the name suggest we allow different tapes for data and wavelet */
+  mspass::BasicTaper *wavelet_taper;
+  mspass::BasicTaper *data_taper;
   double damp;
   mspass::ShapingWavelet shapingwavelet;
   /* This algorithm uses a mix of damping and water level.   Above this floor,
@@ -111,11 +150,18 @@ private:
   //ComplexArray winv;
   /* winv is in FFTDeconOperator*/
   mspass::ComplexArray ao_fft;
+  /* This set of attributes are used for QCMetric calculations */
   /* This holds signal to noise ration (ampllitude) for wavelet with noise
   coming from data.   May need some tweeking for array decon as would tend to
   be overly pessimistic then.  However, it will tend to correctly penalize
   frequencies in the data with poor snr. */
   std::vector<double> wavelet_snr;
+  /* SNR bandbwidth estimates count frequencies with snr above this value */
+  double band_snr_floor;
+  /* This array stores snr band fractions for each component.*/
+  double signal_bandwidth_fraction[3];
+  double peak_snr[3];
+  double regularization_bandwidth_fraction;
   void read_parameters(const mspass::AntelopePf& pf);
   int TestSeismogramInput(Seismogram& d,int comp);
 };
