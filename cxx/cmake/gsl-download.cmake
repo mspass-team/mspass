fa7188e26c4ba7792a03861eae8ed6096023b1ae
wangyinz <yinzhi.wang.cug@gmail.com>
2020-03-13 16:55:01 -0500

    add -fPIC to compile gsl

--- cxx/cmake/gsl-download.cmake
+++ cxx/cmake/gsl-download.cmake
@@ -11,7 +11,7 @@ ExternalProject_Add(
     http://mirror.us-midwest-1.nexcess.net/gnu/gsl/gsl-2.6.tar.gz
   URL_HASH
     SHA256=b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8
-  CONFIGURE_COMMAND ./configure --enable-shared=no --prefix=${PROJECT_BINARY_DIR}
+  CONFIGURE_COMMAND ./configure --enable-shared=no --prefix=${PROJECT_BINARY_DIR} CFLAGS=-fPIC
   BUILD_COMMAND make -j 8
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND make install
