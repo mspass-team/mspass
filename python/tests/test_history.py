c97341433719244794bf1719f20896366ef5493b
wangyinz <yinzhi.wang.cug@gmail.com>
2020-04-23 18:15:21 -0500

    wip: building test for db and history

---
+++ python/tests/test_history.py
@@ -0,0 +1,17 @@
+import pytest
+
+from mspasspy.history import HistoryLogger
+
+# TODO: Need to finish up the test after rearrange the history module. 
+# The naming of classes should follow the same scheme.
+
+class TestHistoryLogger():
+
+    def test_init(self):
+        pass
+
+    def test_register(self):
+        pass
+
+    def test_save(self):
+        pass
