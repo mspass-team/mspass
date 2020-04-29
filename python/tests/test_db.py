9f70bc6aad44b44b1070aa34abd1c523895e39fe
Ian Wang <yinzhi.wang.cug@gmail.com>
2020-04-29 18:52:04 -0500

    test for saveall

--- python/tests/test_db.py
+++ python/tests/test_db.py
@@ -56,18 +56,31 @@ class TestDatabase():
         s1.ns = ts_size
         s1.put('net', 'IU')
 
+        s2 = Seismogram(s1)
+        s2.put('net', 'AK')
+
         self.ts_size = ts_size
         self.sampling_rate = sampling_rate
         self.s1 = s1
+        self.s2 = s2
 
     def test_load3C(self):
         pass
 
     def test_save3C(self):
-        # FIXME: The 'net' key is not saved to database.
+        # FIXME: When something threw an unexpected exception there is no garantee that 'wf_id' is already defined.
         assert self.db.save3C(self.s1, mmode='save', smode='gridfs') == 0
         # FIXME: '_id' should not be used in the find method below. Need to fix the schema.
         assert self.db.wf.count_documents({"_id": ObjectId(self.s1.get('wf_id'))}) == 1
-        doc = self.db.wf.find({"_id": ObjectId(self.s1.get('wf_id'))})[0]
-        assert doc['npts'] == self.ts_size
-        assert doc['delta'] == 1/self.sampling_rate
+        doc1 = self.db.wf.find({"_id": ObjectId(self.s1.get('wf_id'))})[0]
+        assert doc1['npts'] == self.ts_size
+        assert doc1['delta'] == 1/self.sampling_rate
+        with pytest.raises(KeyError):
+            doc1['net']
+        
+        assert self.db.save3C(self.s2, mmode='saveall', smode='gridfs') == 0
+        assert self.db.wf.count_documents({"_id": ObjectId(self.s2.get('wf_id'))}) == 1
+        doc2 = self.db.wf.find({"_id": ObjectId(self.s2.get('wf_id'))})[0]
+        assert doc2['npts'] == self.ts_size
+        assert doc2['delta'] == 1/self.sampling_rate
+        assert doc2['net'] == self.s2.get('net')
