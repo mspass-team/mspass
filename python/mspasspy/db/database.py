746709402c88d5cced1ae2a6b8ffc2f7740f74d2
Ian Wang <yinzhi.wang.cug@gmail.com>
2020-12-26 01:02:55 -0600

    fixed ambiguity in imported module name

--- python/mspasspy/db/database.py
+++ python/mspasspy/db/database.py
@@ -41,11 +41,8 @@ from obspy import Catalog
 
 
 def _read_distributed_data(client_arg, db_name, id, collection, load_history=True, metadata_schema=None):
-    from pymongo import MongoClient
-    from pymongo import database
-    from mspasspy.db import Database
     client = MongoClient(client_arg)
-    db = Database(client, db_name, codec_options=client._codec_options, _store=client._store[db_name])
+    db = Database(client, db_name)
     return db.read_data(id, collection, load_history, metadata_schema)
 
 
