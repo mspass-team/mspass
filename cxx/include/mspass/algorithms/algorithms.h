208dff889af910610d4fdb7a4fa407cc19bcac06
Gary Pavlis <pavlis@indiana.edu>
2021-01-10 09:23:39 -0500

    Add comments for doxygen documentation

--- cxx/include/mspass/algorithms/algorithms.h
+++ cxx/include/mspass/algorithms/algorithms.h
@@ -169,9 +169,97 @@ for zeros in d.
 mspass::seismic::CoreSeismogram sparse_convolve(
     const mspass::seismic::CoreTimeSeries& wavelet,
 		const mspass::seismic::CoreSeismogram& d);
+/*! \brief Combine a grouped set of TimeSeries into one Seismogram.
+
+A Seismogram object is a bundle of TimeSeries objects that define a 
+nonsingular tranformation matrix that can be used to reconstruct vector 
+group motion.   That requires three TimeSeries objects that have 
+define directions that are linearly independent.   This function does not 
+directly test for linear independence but depends upon channel codes
+to assemble one or more bundles needed to build a Seismogram.  The 
+algorithm used here is simple an ONLY works if the inputs have been 
+sorted so the channels groupings of 3 or more channels that belong 
+together.  e.g. if the input defined the following sequence of 
+channel codes:
+  HHE, HHN, HHZ, BHE, BHN, BHZ
+the output would be two Seismogram objects:(1) H bundle and (2) B bundle.
+
+The algorithm also has a limited ability to handle irregular bundles.  
+That is, if the bundle defines less than 3 channels it will be silently 
+dropped.  If the bundle is larger than 3 it will silently attempt to use 
+only the first entry in a group.   Consider this example chan sequence:
+  HHE, HHZ, BHE, BHE, BHN, BHZ
+The H bundle would silentely be dropped.   The B bundle would be assembled 
+from the first BHE in the input vector with BHN and BHZ.  
+
+All bundling uses the CoreTimeSeries constructor for 3 input time series. 
+That constructor has two important requirements to prevent it from throwing 
+an exceptions:  
+1.  Each input must have the hang and vang metadata attributes defined.
+2.  The start and end times of all 3 TimeSeries grouped define an overlapping 
+time.   Note if the start and end times are irregular the returns will 
+be defined ONLY for times common to all three inputs to a group.
+
+Note this function will not throw exceptions unless there is a system 
+error handled by a generic catch. Problems constructing Seismograms for 
+output will result in empty Seismogram objects marked dead BUT with 
+error messages posted to elog.  
+
+ProcessingHistory is handled internally by this function.  If all the 
+components in a group have a nonempty ProcessingHistory the data to link 
+the outputs to the inputs will be posted to ProcessingHistory. 
+
+\param d is the vector of TimeSeries containing data to be bundled. 
+(This could be a members of a sorted Ensemble of a python array in the python 
+api).  
+\param i0 is the first component of the vector to view as part of the group.
+\param iend is the last component of the vector to view as part of the group.
+
+\exception This routine should only throw system generated exceptions. 
+Errors from attempting to construct a Seismogram generate elog messages 
+and kills of an output components. 
+*/
+
 std::vector<mspass::seismic::Seismogram> BundleGroup
     (const std::vector<mspass::seismic::TimeSeries>& d,
                 const size_t i0, const size_t iend);
+/*! \brief Assemble a SeismogramEnsemble from a sorted TimeSeriesEnsemble.
+
+This function can be used to take an (unordered) input ensemble of 
+TimeSeries objects generated from miniseed data and produce an output 
+ensemble of Seismograms produced by bundles linked to the seed name 
+codes net, sta, chan, and loc.   An implicit assumption of the algorithm 
+used here is that the data are a variant of a shot gather and the 
+input ensemble defines one net:sta:chan:loc:time_interval for each 
+record that is to be bundled.   It can only properly handle pure 
+duplicates for a given net:sta:chan:loc combination.  (i.e. if 
+the input has the same TimeSeries defined by net:sta:chan:loc AND
+a common start and end time).   Data with gaps broken into multiple 
+net:sta:chan:loc TimeSeries with different start and end times 
+will produce incomplete results.   That is, Seismograms in the output 
+associated with such inputs will either be killed with an associated 
+error log entry or in the best case truncated to the overlap range of 
+one of the segments with the gap(s) between.  
+
+Irregular start times of any set of TimeSeries forming a single 
+bundle are subject to the same truncation or discard rules described 
+in the related function Bundle3C.   
+
+Finally, it is VERY IMPORTANT to realize that the input ensemble will 
+almost certainly be altered by this algorithm as the very first thing 
+it does is a sequential sort on the seed keys: net, sta, loc, chan 
+(in that order).   Not for convenience in working with MongoDB the 
+null net or loc codes are handled cleanly and treated more or less as 
+if they were an empty string.   The sort order, however, may not be the same
+the as empty and null are not actually treated identically.   
+
+\param d is the input ensemble of TimeSeries to be processed.
+
+\exception This function will throw a MsPASSError if any member of d 
+does not have sta or chan defined (as noted above null net or loc are 
+handled.)
+
+*/
 mspass::seismic::Ensemble<mspass::seismic::Seismogram> bundle_seed_data
     (mspass::seismic::Ensemble<mspass::seismic::TimeSeries>& d);
 }//End mspass::algorithms namespace encapsulation
