1c31038bd897230a6a0ee48ed857113e76663737
Ian Wang <yinzhi.wang.cug@gmail.com>
2021-02-25 01:05:38 -0600

    fixed some obvious bugs

--- scripts/start-mspass.sh
+++ scripts/start-mspass.sh
@@ -14,7 +14,7 @@ if [ $# -eq 0 ]; then
   MY_ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 12 | head -n 1)
   if [ "$MSPASS_SCHEDULER" = "dask" ]; then
     MSPASS_SCHEDULER_CMD="dask-scheduler --port $DASK_SCHEDULER_PORT > ${SPARK_LOG_DIR}/dask-scheduler_log_${MY_ID} 2>&1 & sleep 5"
-    MSPASS_WORKER_CMD="dask-worker spark://$MSPASS_SCHEDULER_ADDRESS:$DASK_SCHEDULER_PORT > ${SPARK_LOG_DIR}/dask-worker_log_${MY_ID} 2>&1 &"
+    MSPASS_WORKER_CMD="dask-worker --local-directory $SPARK_LOG_DIR tcp://$MSPASS_SCHEDULER_ADDRESS:$DASK_SCHEDULER_PORT > ${SPARK_LOG_DIR}/dask-worker_log_${MY_ID} 2>&1 &"
   elif [ "$MSPASS_SCHEDULER" = "spark" ]; then
     MSPASS_SCHEDULER_CMD="$SPARK_HOME/sbin/start-master.sh"
     MSPASS_WORKER_CMD="$SPARK_HOME/sbin/start-slave.sh spark://$MSPASS_SCHEDULER_ADDRESS:$SPARK_MASTER_PORT"
@@ -31,8 +31,8 @@ if [ $# -eq 0 ]; then
     mongod --port $MONGODB_PORT --dbpath $MONGO_DATA --logpath $MONGO_LOG --bind_ip_all
   elif [ "$MSPASS_ROLE" = "dbmanager" ]; then
     MONGODB_CONFIG_PORT=$(($MONGODB_PORT+1))
-    [[ -d $MONGO_DATA ]] || mkdir $MONGO_DATA
-    mongod --port $MONGODB_CONFIG_PORT --configsvr --replSet configserver --dbpath $MONGO_DATA --logpath ${MONGO_LOG}_config --bind_ip_all &
+    [[ -d ${MONGO_DATA}_config ]] || mkdir ${MONGO_DATA}_config
+    mongod --port $MONGODB_CONFIG_PORT --configsvr --replSet configserver --dbpath ${MONGO_DATA}_config --logpath ${MONGO_LOG}_config --bind_ip_all &
     sleep 5
     mongo --port $MONGODB_CONFIG_PORT --eval \
       "rs.initiate({_id: \"configserver\", configsvr: true, version: 1, members: [{ _id: 0, host : \"127.0.0.1:$MONGODB_CONFIG_PORT\" }]})"
@@ -41,9 +41,11 @@ if [ $# -eq 0 ]; then
     for i in ${MSPASS_SHARD_LIST[@]}; do 
       mongo --port $MONGODB_PORT --eval "sh.addShard(\"${i}\")"
     done
+    tail -f /dev/null
   elif [ "$MSPASS_ROLE" = "shard" ]; then
-    [[ -d $MONGO_DATA ]] || mkdir $MONGO_DATA
-    mongod --port $MONGODB_PORT --shardsvr --dbpath $MONGO_DATA --logpath ${MONGO_LOG}_${MY_ID} --bind_ip_all
+    [[ -n $MSPASS_SHARD_ID ]] || MSPASS_SHARD_ID=$MY_ID
+    [[ -d ${MONGO_DATA}_shard_${MSPASS_SHARD_ID} ]] || mkdir ${MONGO_DATA}_shard_${MSPASS_SHARD_ID}
+    mongod --port $MONGODB_PORT --shardsvr --dbpath ${MONGO_DATA}_shard_${MSPASS_SHARD_ID} --logpath ${MONGO_LOG}_shard_${MSPASS_SHARD_ID}  --bind_ip_all
   elif [ "$MSPASS_ROLE" = "scheduler" ]; then
     eval $MSPASS_SCHEDULER_CMD
     tail -f /dev/null
