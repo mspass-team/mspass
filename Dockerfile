be9bd123b51bb4d3649df6d0149840921a6df838
Ian Wang <yinzhi.wang.cug@gmail.com>
2021-03-02 22:07:51 -0600

    add password setting to jupyter; reorganize directories for logs and files; shards are created as replica set now; fix some bugs

--- Dockerfile
+++ Dockerfile
@@ -89,7 +89,7 @@ ADD python /mspass/python
 RUN pip3 install /mspass -v
 
 # Install Jupyter notebook
-RUN pip --no-cache-dir install notebook==6.2.0
+RUN pip3 --no-cache-dir install notebook==6.2.0
 
 # Tini operates as a process subreaper for jupyter.
 ENV TINI_VERSION v0.19.0
@@ -109,4 +109,4 @@ ENV JUPYTER_PORT 8888
 ENV MSPASS_ROLE all
 ENV MSPASS_SCHEDULER dask
 
-ENTRYPOINT ["/usr/sbin/start-mspass.sh"]
+ENTRYPOINT ["/usr/sbin/tini", "-g", "--", "/usr/sbin/start-mspass.sh"]
--- scripts/start-mspass.sh
+++ scripts/start-mspass.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
+# If running with docker use /home, else use pwd to store all data and logs
 if grep docker /proc/1/cgroup -qa; then
-  export SPARK_LOG_DIR=/home
-  MONGO_DATA=/home/data
-  MONGO_LOG=/home/mongo_log
+  _MY_HOME=/home
 else
-  export SPARK_LOG_DIR=$PWD
-  MONGO_DATA=${PWD%/}/data
-  MONGO_LOG=${PWD%/}/mongo_log
+  _MY_HOME=${PWD%/}
 fi
+
+MSPASS_DB_DIR=${_MY_HOME}/db
+MSPASS_LOG_DIR=${_MY_HOME}/logs
+MSPASS_WORKER_DIR=${_MY_HOME}/work
+# Note that only log is required for all roles. Other dirs will be created later when needed.
+[[ -d $MSPASS_LOG_DIR ]] || mkdir -p $MSPASS_LOG_DIR
+
+MONGO_DATA=${MSPASS_DB_DIR}/data
+MONGO_LOG=${MSPASS_LOG_DIR}/mongo_log
+export SPARK_WORKER_DIR=${MSPASS_WORKER_DIR}
+export SPARK_LOG_DIR=${MSPASS_LOG_DIR}
 
 if [ $# -eq 0 ]; then
 
   function start_mspass_frontend {
+    NOTEBOOK_ARGS="--notebook-dir=${_MY_HOME} --port=${JUPYTER_PORT} --no-browser --ip=0.0.0.0 --allow-root"
+    # if MSPASS_JUPYTER_PWD is not set, notebook will generate a default token
+    if [[ ! -z ${MSPASS_JUPYTER_PWD+x} ]]; then
+      # we rely on jupyter's python function to hash the password
+      MSPASS_JUPYTER_PWD_HASHED=$(python3 -c "from notebook.auth import passwd; print(passwd('${MSPASS_JUPYTER_PWD}'))")
+      NOTEBOOK_ARGS="${NOTEBOOK_ARGS} --NotebookApp.password=${MSPASS_JUPYTER_PWD_HASHED}" 
+    fi
     if [ "$MSPASS_SCHEDULER" = "spark" ]; then
       export PYSPARK_DRIVER_PYTHON=jupyter
-      export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=${SPARK_LOG_DIR} --port=${JUPYTER_PORT} --no-browser --ip=0.0.0.0 --allow-root"
-      /usr/sbin/tini -g -- pyspark \
+      export PYSPARK_DRIVER_PYTHON_OPTS="notebook ${NOTEBOOK_ARGS}"
+      pyspark \
         --conf "spark.mongodb.input.uri=mongodb://${MSPASS_DB_ADDRESS}:${MONGODB_PORT}/test.misc" \
         --conf "spark.mongodb.output.uri=mongodb://${MSPASS_DB_ADDRESS}:${MONGODB_PORT}/test.misc" \
         --conf "spark.master=spark://${MSPASS_SCHEDULER_ADDRESS}:${SPARK_MASTER_PORT}" \
         --packages org.mongodb.spark:mongo-spark-connector_2.12:3.0.0
     else # if [ "$MSPASS_SCHEDULER" = "dask" ]
       export DASK_SCHEDULER_ADDRESS=${MSPASS_SCHEDULER_ADDRESS}:${DASK_SCHEDULER_PORT}
-      /usr/sbin/tini -g -- notebook --notebook-dir=${SPARK_LOG_DIR} --port=${JUPYTER_PORT} --no-browser --ip=0.0.0.0 --allow-root
+      jupyter notebook ${NOTEBOOK_ARGS}
     fi
   }
 
@@ -32,16 +47,16 @@ if [ $# -eq 0 ]; then
     MSPASS_SCHEDULER_CMD='$SPARK_HOME/sbin/start-master.sh'
     MSPASS_WORKER_CMD='$SPARK_HOME/sbin/start-slave.sh spark://$MSPASS_SCHEDULER_ADDRESS:$SPARK_MASTER_PORT'
   else # if [ "$MSPASS_SCHEDULER" = "dask" ]
-    MSPASS_SCHEDULER_CMD='dask-scheduler --port $DASK_SCHEDULER_PORT > ${SPARK_LOG_DIR}/dask-scheduler_log_${MY_ID} 2>&1 & sleep 5'
-    MSPASS_WORKER_CMD='dask-worker --local-directory $SPARK_LOG_DIR tcp://$MSPASS_SCHEDULER_ADDRESS:$DASK_SCHEDULER_PORT > ${SPARK_LOG_DIR}/dask-worker_log_${MY_ID} 2>&1 &'
+    MSPASS_SCHEDULER_CMD='dask-scheduler --port $DASK_SCHEDULER_PORT > ${MSPASS_LOG_DIR}/dask-scheduler_log_${MY_ID} 2>&1 & sleep 5'
+    MSPASS_WORKER_CMD='dask-worker --local-directory $MSPASS_WORKER_DIR tcp://$MSPASS_SCHEDULER_ADDRESS:$DASK_SCHEDULER_PORT > ${MSPASS_LOG_DIR}/dask-worker_log_${MY_ID} 2>&1 &'
   fi
 
   if [ "$MSPASS_ROLE" = "db" ]; then
-    [[ -d $MONGO_DATA ]] || mkdir $MONGO_DATA
+    [[ -d $MONGO_DATA ]] || mkdir -p $MONGO_DATA
     mongod --port $MONGODB_PORT --dbpath $MONGO_DATA --logpath $MONGO_LOG --bind_ip_all
   elif [ "$MSPASS_ROLE" = "dbmanager" ]; then
     MONGODB_CONFIG_PORT=$(($MONGODB_PORT+1))
-    [[ -d ${MONGO_DATA}_config ]] || mkdir ${MONGO_DATA}_config
+    [[ -d ${MONGO_DATA}_config ]] || mkdir -p ${MONGO_DATA}_config
     mongod --port $MONGODB_CONFIG_PORT --configsvr --replSet configserver --dbpath ${MONGO_DATA}_config --logpath ${MONGO_LOG}_config --bind_ip_all &
     sleep 5
     mongo --port $MONGODB_CONFIG_PORT --eval \
@@ -54,23 +69,33 @@ if [ $# -eq 0 ]; then
     tail -f /dev/null
   elif [ "$MSPASS_ROLE" = "shard" ]; then
     [[ -n $MSPASS_SHARD_ID ]] || MSPASS_SHARD_ID=$MY_ID
-    [[ -d ${MONGO_DATA}_shard_${MSPASS_SHARD_ID} ]] || mkdir ${MONGO_DATA}_shard_${MSPASS_SHARD_ID}
-    mongod --port $MONGODB_PORT --shardsvr --dbpath ${MONGO_DATA}_shard_${MSPASS_SHARD_ID} --logpath ${MONGO_LOG}_shard_${MSPASS_SHARD_ID}  --bind_ip_all
+    [[ -d ${MONGO_DATA}_shard_${MSPASS_SHARD_ID} ]] || mkdir -p ${MONGO_DATA}_shard_${MSPASS_SHARD_ID}
+    # Note that we have to create a one-member replica set here 
+    # because certain pymongo API will use "retryWrites=true" 
+    # and thus trigger an error.
+    mongod --port $MONGODB_PORT --shardsvr --replSet ${HOSTNAME} --dbpath ${MONGO_DATA}_shard_${MSPASS_SHARD_ID} --logpath ${MONGO_LOG}_shard_${MSPASS_SHARD_ID}  --bind_ip_all &
+    sleep 5
+    mongo --port $MONGODB_PORT --eval \
+      "rs.initiate({_id: \"${HOSTNAME}\", version: 1, members: [{ _id: 0, host : \"$HOSTNAME:$MONGODB_PORT\" }]})"
+    tail -f /dev/null
   elif [ "$MSPASS_ROLE" = "scheduler" ]; then
     eval $MSPASS_SCHEDULER_CMD
     tail -f /dev/null
   elif [ "$MSPASS_ROLE" = "worker" ]; then
+    [[ -d $MSPASS_WORKER_DIR ]] || mkdir -p $MSPASS_WORKER_DIR
     eval $MSPASS_WORKER_CMD
     tail -f /dev/null
   elif [ "$MSPASS_ROLE" = "frontend" ]; then
     start_mspass_frontend
   else # if [ "$MSPASS_ROLE" = "all" ]
-    MSPASS_SCHEDULER_ADDRESS=127.0.0.1
+    MSPASS_DB_ADDRESS=$HOSTNAME
+    MSPASS_SCHEDULER_ADDRESS=$HOSTNAME
     eval $MSPASS_SCHEDULER_CMD
     eval $MSPASS_WORKER_CMD
-    [[ -d $MONGO_DATA ]] || mkdir $MONGO_DATA
+    [[ -d $MONGO_DATA ]] || mkdir -p $MONGO_DATA
     mongod --port $MONGODB_PORT --dbpath $MONGO_DATA --logpath $MONGO_LOG --bind_ip_all &
     start_mspass_frontend
+  fi
 else
   docker-entrypoint.sh $@
 fi
