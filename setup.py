ab49eab7d8b6c46b57df9c90207a60c509bc35b6
Ian Wang <yinzhi.wang.cug@gmail.com>
2021-04-11 00:26:56 -0500

    speed up workflow

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class CMakeBuild(build_ext):
             build_args += ['--', '/m']
         else:
             cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j4']
+            build_args += ['--', '-j']
 
         env = os.environ.copy()
         env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
