96c475bf2bb223a15dd4777c446b4c5af0888328
haruming <474285697@qq.com>
2021-04-19 00:01:30 -0500

    add hostname to mongos

--- scripts/start-mspass.sh
+++ scripts/start-mspass.sh
@@ -61,8 +61,9 @@ if [ $# -eq 0 ]; then
     sleep 5
     mongo --port $MONGODB_CONFIG_PORT --eval \
       "rs.initiate({_id: \"configserver\", configsvr: true, version: 1, members: [{ _id: 0, host : \"$HOSTNAME:$MONGODB_CONFIG_PORT\" }]})"
-    mongos --port $MONGODB_PORT --configdb configserver/$HOSTNAME:$MONGODB_CONFIG_PORT --logpath ${MONGO_LOG}_router --bind_ip_all &
-    sleep 5
+    mongos --host $HOSTNAME --port $MONGODB_PORT --configdb configserver/$HOSTNAME:$MONGODB_CONFIG_PORT --logpath ${MONGO_LOG}_router --bind_ip_all &
+    # sleep longer
+    sleep 15
     for i in ${MSPASS_SHARD_LIST[@]}; do 
       echo ${i}
       mongo --host $HOSTNAME --port $MONGODB_PORT --eval "sh.addShard(\"${i}\")"
