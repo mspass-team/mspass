76ee123bb26b310787086071262503692b8cee9e
Zhengtang <zhengtang>
2021-10-13 11:07:28 -0500

    Refactor RFdecon   Add __str()__ and __repr()__ to RFdeconProcessor   Add a testcase for RFdecon

--- python/tests/algorithms/test_RFdeconProcessor.py
+++ python/tests/algorithms/test_RFdeconProcessor.py
@@ -12,8 +12,8 @@ from helper import (get_live_seismogram,
                     get_sin_timeseries,
                     get_live_timeseries_ensemble,
                     get_live_seismogram_ensemble)
-
-from mspasspy.algorithms.RFdeconProcessor import RFdeconProcessor
+from mspasspy.algorithms.window import WindowData
+from mspasspy.algorithms.RFdeconProcessor import RFdeconProcessor, RFdecon
 
 def test_RFdeconProcessor():
     decon_processor = RFdeconProcessor(alg="MultiTaperXcor")
@@ -46,3 +46,29 @@ def test_RFdeconProcessor():
     result1 = np.array(decon_processor.apply())
     result2 = np.array(decon_processor_copy.apply())
     assert all(abs(a-b) < 1e-6 for a,b in zip(result1, result2))
+
+def test_RFdeconr():
+    seis1 = get_live_seismogram(71, 2.0)
+    seis1.t0 = -5
+
+    seis2 = get_live_seismogram(71, 2.0)
+    seis2.t0 = -5
+    for i in range(3):
+        for j in range(seis2.npts):
+            seis2.data[i, j] = seis1.data[i, j]
+
+    processor = RFdeconProcessor()
+    processor.loaddata(seis1)
+    processor.loadnoise(seis1, window=True)
+    processor.loadwavelet(seis1, window=True)
+    result1 = WindowData(seis1, processor.dwin.start, processor.dwin.end)
+    for k in range(3):
+        processor.loaddata(result1, component=k)
+        x = processor.apply()
+        for i in range(seis1.npts):
+            result1.data[k, i] = x[i]
+
+    result2 = RFdecon(seis2)
+
+    for k in range(3):
+        assert all(abs(a-b) < 1e-6 for a,b in zip(result1.data[k], result2.data[k]))
