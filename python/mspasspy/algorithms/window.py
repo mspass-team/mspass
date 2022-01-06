50c9a534eee0701a0463408f3e18a3cc4813d43d
Gary Pavlis <pavlis@indiana.edu>
2022-01-06 14:29:56 -0500

    Fix typo

--- python/mspasspy/algorithms/window.py
+++ python/mspasspy/algorithms/window.py
@@ -291,10 +291,10 @@ def WindowData(d, win_start, win_end, t0shift=None,
     try:
         # This handler duplicates an error test in the WindowData C code but
         # it will be more efficient to handle it here.
-        if tscut.start < d.t0 or twcut.end > d.endtime():
+        if twcut.start < d.t0 or twcut.end > d.endtime():
             detailline = 'Window range: {wst},{wet}  Data range:  {dst},{det}'.format(
-                wst=tscut.start,
-                wet=tscut.end,
+                wst=twcut.start,
+                wet=twcut.end,
                 dst=d.t0,
                 det=d.endtime()
             )
