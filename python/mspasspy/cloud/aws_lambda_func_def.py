d6b6b6041a245b3b157946bb8a58577b1d361a08
Zhengtang <yangzhengtang@utexas.edu>
2022-01-10 04:07:17 -0600

    Add support for aws lambda.

---
+++ python/mspasspy/cloud/aws_lambda_func_def.py
@@ -0,0 +1,10 @@
+from obspy.core.stream import Stream
+from obspy.core import UTCDateTime
+
+def lambda_func(input_path, event):
+    # after processing, return output_path
+    # arg1 = event['arg1']
+    # arg2 = event['arg2']
+    # output_path = preprocess(intput_path)
+    # return output_path
+    pass
