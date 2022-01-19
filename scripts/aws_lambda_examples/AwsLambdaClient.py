ad1078fba1fae8e1c0b25f1d7b585f9879738adc
Zhengtang <yangzhengtang@utexas.edu>
2022-01-19 09:16:58 -0600

    Add comments.

--- scripts/aws_lambda_examples/AwsLambdaClient.py
+++ scripts/aws_lambda_examples/AwsLambdaClient.py
@@ -10,7 +10,29 @@ import json
 
 # Settings for aws lambda uploading and calling.
 class AwsLambdaClient:
-    def __init__(self, aws_access_key_id, aws_secret_access_key, lambda_upload_bucket, lambda_iam_role, region_name):
+    """
+    A class to store the aws user info, and to handle the lambda related functions,
+    including creating a lambda function, and calling a lambda function.
+    """
+
+    def __init__(
+        self,
+        aws_access_key_id,
+        aws_secret_access_key,
+        lambda_upload_bucket,
+        lambda_iam_role,
+        region_name,
+    ):
+        """
+        Basic constructor for AwsLambdaClient, require the users' aws info. Those info would be used
+        to establish aws connection and create s3 and lambda clients.
+
+        :param aws_access_key_id: A part of the credentials to authenticate the user
+        :param aws_secret_access_key: A part of the credentials to authenticate the user
+        :param lambda_upload_bucket: The name of s3 bucket where the zip file containing lambda function would be uploaded to
+        :param lambda_iam_role: The IAM role that has lambda and s3 permission, required to create lambda function.
+        :param region_name: AWS region where the lambda function will be deployed.
+        """
         self.aws_access_key_id = aws_access_key_id
         self.aws_secret_access_key = aws_secret_access_key
         self.lambda_upload_bucket = lambda_upload_bucket
@@ -18,18 +40,29 @@ class AwsLambdaClient:
         self.region_name = region_name
 
     def create_aws_client(self, client_type):
-        if client_type not in ['lambda', 's3']:
+        """
+        Establish the connection with AWS, and create the client.
+
+        :param client_type: The service name user would like to use, in this class, only 'lambda' and 's3' are supported.
+        :return: The lambda or s3 client.
+        """
+        if client_type not in ["lambda", "s3"]:
             raise Exception("Undefined client type, please use 'lambda' or 's3'.")
-        aws_session = boto3.Session(aws_access_key_id = self.aws_access_key_id, aws_secret_access_key = self.aws_secret_access_key, region_name = self.region_name)
+        aws_session = boto3.Session(
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            region_name=self.region_name,
+        )
         client = aws_session.client(client_type, self.region_name)
         return client
-    
+
     @staticmethod
     def _updateZip(zip_path, filename):
-        """ 
-        zip = zipfile.ZipFile(zip_path,'a')
-        zip.write(filename, os.path.basename(filename))
-        zip.close() 
+        """
+        Update a file in the zip archive. This function has the same functionaliy as the command "zip -r ${zip_path} ${filename}".
+
+        :param zip_path: The path of zip archive.
+        :param filename: The path of file to pack in the zip archive and update.
         """
         if not os.path.exists(zip_path):
             raise Exception("Missing zip file: " + zip_path, "Fatal")
@@ -38,9 +71,9 @@ class AwsLambdaClient:
 
         tempdir = tempfile.mkdtemp()
         try:
-            tempname = os.path.join(tempdir, 'new.zip')
-            with zipfile.ZipFile(zip_path, 'r') as zipread:
-                with zipfile.ZipFile(tempname, 'w') as zipwrite:
+            tempname = os.path.join(tempdir, "new.zip")
+            with zipfile.ZipFile(zip_path, "r") as zipread:
+                with zipfile.ZipFile(tempname, "w") as zipwrite:
                     for item in zipread.infolist():
                         if item.filename != filename:
                             data = zipread.read(item.filename)
@@ -49,63 +82,76 @@ class AwsLambdaClient:
         finally:
             shutil.rmtree(tempdir)
 
-        with zipfile.ZipFile(zip_path, 'a') as z:
+        with zipfile.ZipFile(zip_path, "a") as z:
             z.write(filename)
-        """ 
-        tempdir = tempfile.mkdtemp() 
-        with zipfile.ZipFile(zip_path, 'r') as old_zip:
-            old_zip.extractall(tempdir)
-        os.remove(zip_path)
-        shutil.copyfile(filename, os.path.join(tempdir, os.path.basename(filename)))
-
-        with zipfile.ZipFile(zip_path, 'w') as new_zip:
-            for folderName, subfolders, filenames in os.walk(tempdir):
-                for filename in filenames:
-                    filePath = os.path.join(folderName, filename)
-                    new_zip.write(filePath, os.path.basename(filePath)) 
-        """
 
     def create_lambda_function(self, function_name):
-        s3_client = self.create_aws_client('s3')
-        self._updateZip('base.zip', 'aws_lambda_func_def.py')
-        s3_client.put_object(Key='base.zip', \
-                            Bucket=self.lambda_upload_bucket, \
-                            Body=open('base.zip', 'rb') \
-                            )
+        """
+        Upload a user-defined lambda function to the AWS.
 
-        lambda_client = self.create_aws_client('lambda')
+        :param functionName: the name of the lambda function, which will be used to call
+        """
+
+        s3_client = self.create_aws_client("s3")
+        self._updateZip("base.zip", "aws_lambda_func_def.py")
+        s3_client.put_object(
+            Key="base.zip",
+            Bucket=self.lambda_upload_bucket,
+            Body=open("base.zip", "rb"),
+        )
+
+        lambda_client = self.create_aws_client("lambda")
         lambda_client.create_function(
             FunctionName=function_name,
-            Runtime='python3.7',
+            Runtime="python3.7",
             Role=self.lambda_iam_role,
-            Handler='process.handler',
-            Code={
-                'S3Bucket': self.lambda_upload_bucket,
-                'S3Key': 'base.zip'
-            },
-            Description='',
+            Handler="process.handler",
+            Code={"S3Bucket": self.lambda_upload_bucket, "S3Key": "base.zip"},
+            Description="",
             Timeout=300,
             MemorySize=1024,
-            Publish=True
+            Publish=True,
         )
 
     def call_lambda_function(self, function_name, request):
-        lambda_client = self.create_aws_client('lambda')
-        response = lambda_client.invoke(
-                FunctionName=function_name,
-                InvocationType='RequestResponse',
-                LogType='Tail',
-                Payload=json.dumps(request))
+        """
+        Call an existing lambda function.
 
-        response_payload = json.loads(response['Payload'].read())
-        ret_type = response_payload['ret_type']
-        if(ret_type == 'key'):  #   The output file is saved to another bucket (s3_output_bucket).
-            ret_value = response_payload['ret_value']
-            print("The window given is too large, can't be returned immediately,\nPlease check {}".format(ret_value))
-        elif(ret_type == 'content'):
-            ret_value = base64.b64decode(response_payload['ret_value'].encode("utf-8"))
+        :param functionName: the name of the lambda function to call.
+        :param request: an dictionary that contains all the arguments passed to the lambda function. It will be dumped as a json string and then used as the payload of request.
+        It should at least contain four elements: ‘src_bucket’, ‘dst_bucket’, ‘src_key’, ‘dst_key’. They will indicate the input and output object of this lambda call.
+        :return: a dict that contain two elements:
+            1) ret_type: two possible value: ‘key’ or ‘value’,
+                ‘key’ means that the output object is saved to some place in s3.
+                ‘value’ means that the output object is directly returned through payload
+            2) ret_value:
+                If ret_type=’key’, ret_value will be the key of the output object in s3.
+                If ret_type=’value’, ret_value will be the bytes of the returning object.
+        """
+
+        lambda_client = self.create_aws_client("lambda")
+        response = lambda_client.invoke(
+            FunctionName=function_name,
+            InvocationType="RequestResponse",
+            LogType="Tail",
+            Payload=json.dumps(request),
+        )
+
+        response_payload = json.loads(response["Payload"].read())
+        ret_type = response_payload["ret_type"]
+        if (
+            ret_type == "key"
+        ):  #   The output file is saved to another bucket (s3_output_bucket).
+            ret_value = response_payload["ret_value"]
+            print(
+                "The window given is too large, can't be returned immediately,\nPlease check {}".format(
+                    ret_value
+                )
+            )
+        elif ret_type == "content":
+            ret_value = base64.b64decode(response_payload["ret_value"].encode("utf-8"))
         else:
             raise Exception("Undefined return type when calling " + function, "Fatal")
-        
-        ret_dict = {'return_type':ret_type, 'ret_value': ret_value}
+
+        ret_dict = {"return_type": ret_type, "ret_value": ret_value}
         return ret_dict
--- scripts/aws_lambda_examples/aws_lambda_func_def.py
+++ scripts/aws_lambda_examples/aws_lambda_func_def.py
@@ -4,38 +4,55 @@ import obspy
 
 '''
 def lambda_func(input_path, event):
-    # after processing, return output_path
-    # arg1 = event['arg1']
-    # arg2 = event['arg2']
-    # output_path = preprocess(intput_path)
-    # return output_path
-    pass
+    """
+    The template for user-defined lambda function.
+
+    The maximum running time of one execution is 15 mins, so the lambda function can’t be used to do heavy work. 
+    The main point of lambda function is to help do some trivial preprocessing on data on aws s3. If some heavy
+    calculations are to be done, the better way is to download the data and do it locally.
+
+    :param input_path: The path of the input file.
+    :param event: The dictionary passed by "process" function, should be the same dict in request used when calling
+        "AwsLambdaClient.call_lambda_function". The user-specified parameters can be extracted as follows:
+            arg1 = event['arg1']
+            arg2 = event['arg2']
+    :return: a dict that contain two elements:
+        1) ret_type: two possible value: ‘key’ or ‘value’, 
+            ‘key’ means that the output object is saved to some place in s3.
+            ‘value’ means that the output object is directly returned through payload
+        2) ret_value:
+            If ret_type=’key’, ret_value will be the key of the output object in s3.
+            If ret_type=’value’, ret_value will be the bytes of the returning object.
+    """
 '''
 
+
 def lambda_func(input_path, event):
+    # A simple example. This function will do the timewindow on the input file.
+    # Users should replace this function with their own function following the template above.
     duration = -1
     t0shift = 0
-    if 'duration' in event:
-        duration = event['duration']
-    if 't0shift' in event:
-        t0shift = event['t0shift']
-        
+    if "duration" in event:
+        duration = event["duration"]
+    if "t0shift" in event:
+        t0shift = event["t0shift"]
+
     basename = os.path.basename(input_path)
     (filename, ext) = os.path.splitext(basename)
     filename = filename + "_{}_{}".format(duration, t0shift) + ext
-    outfile = '/tmp/' + filename
+    outfile = "/tmp/" + filename
 
-    if(duration == -1): #   Do nothing, just copy the file
-        print('Not window, because not given params')
+    if duration == -1:  #   Do nothing, just copy the file
+        print("Not window, because not given params")
         shutil.copyfile(input_path, outfile)
         return outfile
-    
-    print('Start handleing ', input_path)
+
+    print("Start handleing ", input_path)
     st = obspy.read(input_path)
-    start_time = st[0].stats['starttime'] + t0shift
+    start_time = st[0].stats["starttime"] + t0shift
     end_time = start_time + duration
     st.trim(start_time, end_time)
 
-    print('Window done, writing', outfile)
-    st.write(outfile, 'MSEED')
+    print("Window done, writing", outfile)
+    st.write(outfile, "MSEED")
     return outfile
