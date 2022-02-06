f48550957e23bc6a249cafe1397bc81ac0fc980d
zhengtang <yangzhengtang@utexas.edu>
2022-02-06 15:41:07 +0000

    Add testcase for window.py

--- python/mspasspy/algorithms/window.py
+++ python/mspasspy/algorithms/window.py
@@ -371,9 +371,28 @@ def WindowData(
         d.kill()
         return d
 
+
 @mspass_func_wrapper
-def WindowData_with_duration(d, duration, t0shift=None,
-               object_history=False, alg_name='scale', alg_id=None, dryrun=False):
+def WindowData_with_duration(
+    d,
+    duration,
+    t0shift=None,
+    object_history=False,
+    alg_name="scale",
+    alg_id=None,
+    dryrun=False,
+):
+    if duration < 0:
+        detailline = "Window duration: {dur}  Data range:  {dst},{det}".format(
+            dur=duration, dst=d.t0, det=d.endtime()
+        )
+        d.elog.log_error(
+            "WindowData",
+            "Duration is a negative number.\n" + detailline,
+            ErrorSeverity.Invalid,
+        )
+        d.kill()
+        return d
     win_start = d.t0 + 1
     win_end = win_start + duration
     if d.dead():
@@ -385,14 +404,14 @@ def WindowData_with_duration(d, duration, t0shift=None,
         # This handler duplicates an error test in the WindowData C code but
         # it will be more efficient to handle it here.
         if win_start < d.t0 or win_end > d.endtime():
-            detailline = 'Window range: {wst},{wet}  Data range:  {dst},{det}'.format(
-                wst=win_start,
-                wet=win_end,
-                dst=d.t0,
-                det=d.endtime()
+            detailline = "Window range: {wst},{wet}  Data range:  {dst},{det}".format(
+                wst=win_start, wet=win_end, dst=d.t0, det=d.endtime()
             )
-            d.elog.log_error("WindowData", "Data range is smaller than window range\n"+detailline,
-                             ErrorSeverity.Invalid)
+            d.elog.log_error(
+                "WindowData",
+                "Data range is smaller than window range\n" + detailline,
+                ErrorSeverity.Invalid,
+            )
             d.kill()
             return d
         if isinstance(d, TimeSeries):
@@ -403,7 +422,8 @@ def WindowData_with_duration(d, duration, t0shift=None,
             return dcut
         else:
             raise RuntimeError(
-                "WindowData:  Invalid input data type received="+str(type(d)))
+                "WindowData:  Invalid input data type received=" + str(type(d))
+            )
     except MsPASSError as err:
         d.log_error("WindowData", str(err), ErrorSeverity.Invalid)
         d.kill()
