b27ec50b3e7f562c88ed9442ac4dc36dc27ee1d9
Gary Pavlis <pavlis@indiana.edu>
2022-02-27 07:10:25 -0500

    Fix subtle error created by incorrect use of virtual inheritance

--- cxx/src/lib/seismic/TimeSeries.cc
+++ cxx/src/lib/seismic/TimeSeries.cc
@@ -25,10 +25,11 @@ TimeSeries::TimeSeries(const BasicTimeSeries& b, const Metadata& m,
 {
   this->s=d;
 }
-TimeSeries::TimeSeries(const Metadata& md) : Metadata(md),ProcessingHistory()
+TimeSeries::TimeSeries(const Metadata& md) : ProcessingHistory()
 {
     mlive=false;
     try {
+        this->Metadata::operator=(md);
         /* Names used are from mspass defintions as of Jan 2020.
         We don't need to call the set methods for these attributes as they
         would add the overhead of setting delta, startime, and npts to the
@@ -57,16 +58,6 @@ TimeSeries::TimeSeries(const Metadata& md) : Metadata(md),ProcessingHistory()
             this->force_t0_shift(t0shift);
           }
         }
-        /* Maintenance issue:   The first version of this code had this
-        construct to set npts:
-        long ns = this->get_long(SEISMICMD_npts);
-        For reasons I (glp) could not figure out ns was always returned as 0
-        when extracted from this.   The Metadata(md) call in the first line of
-        this constructor should have set npts but it did not do so for some
-        reason.  The following is a workaround that is totally equivalent but
-        why it is necessary is not at all clear.  I am putting this here as this
-        could come back to bite us.
-        */
         long int ns = md.get_long(SEISMICMD_npts);
         /* this CoreTimeSeries method sets the npts attribute and
         initializes the s buffer to all zeros */
