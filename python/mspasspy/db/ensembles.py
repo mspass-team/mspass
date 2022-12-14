737598773de9e0ce132f43ea98d96b8c1c3fa41c
wangyinz <wangyinz@users.noreply.github.com>
2022-12-14 22:59:43 +0000

    :art: Format Python code with psf/black

--- python/mspasspy/db/ensembles.py
+++ python/mspasspy/db/ensembles.py
@@ -50,120 +50,120 @@ def TimeIntervalReader(
     dryrun=False,
 ) -> list:
     """
-    A common form of gather when handling continous data with UTC timing 
-    is to carve out fixed time window.   Two common but different uses 
-    are (1) carving out an interval of time based on an event origin time, 
-    and (2) extracting a series of windows in a loop for block processing 
-    like noise correlation or spectorgrams.   The later demand a lot of 
-    efficiency in this process as that kind of process can repeat an 
-    algorithm like this thousands of times.   Reading continuous data 
-    always has to handle two properties of real data that this function 
-    hides behind the implementation.  (1) in all but trivially short 
-    experiments the length of recording for any given instrument is 
-    far too large to fit in memory.  Data are thus always broken into 
-    fixed chunks.  In most cases that has evolved to mean day-long 
-    chunks.  (2) A large fraction of data have recording gaps for a 
-    long list of reasons.  The second folds into the first in a less 
+    A common form of gather when handling continous data with UTC timing
+    is to carve out fixed time window.   Two common but different uses
+    are (1) carving out an interval of time based on an event origin time,
+    and (2) extracting a series of windows in a loop for block processing
+    like noise correlation or spectorgrams.   The later demand a lot of
+    efficiency in this process as that kind of process can repeat an
+    algorithm like this thousands of times.   Reading continuous data
+    always has to handle two properties of real data that this function
+    hides behind the implementation.  (1) in all but trivially short
+    experiments the length of recording for any given instrument is
+    far too large to fit in memory.  Data are thus always broken into
+    fixed chunks.  In most cases that has evolved to mean day-long
+    chunks.  (2) A large fraction of data have recording gaps for a
+    long list of reasons.  The second folds into the first in a less
     obvious way because of another practical data issue;  timing problems.
-    When an instrument's timing system fails the internal clock will 
-    drift away from the external reference time.  When the instrument 
-    again receives a timing signal the next packet written 
+    When an instrument's timing system fails the internal clock will
+    drift away from the external reference time.  When the instrument
+    again receives a timing signal the next packet written
     (note all modern data are collected in digital packets like miniseed)
-    may have a "time tear".  That means the time stamp is off by more than 
-    1/2 sample interval from the time computed from the last valid time 
-    stamp and the nominal sample interval.   When reading continuous 
-    data that kind of anomaly will appear like one of two things: (1) 
-    a data gap if the time tear is a forward jump in time, or (2) a data 
-    overlap if the time tear is a backward jump in time.   In MsPASS 
-    our miniseed indexer detects time tears and data gaps 
+    may have a "time tear".  That means the time stamp is off by more than
+    1/2 sample interval from the time computed from the last valid time
+    stamp and the nominal sample interval.   When reading continuous
+    data that kind of anomaly will appear like one of two things: (1)
+    a data gap if the time tear is a forward jump in time, or (2) a data
+    overlap if the time tear is a backward jump in time.   In MsPASS
+    our miniseed indexer detects time tears and data gaps
     (The algorithm just checks for greater than 1/2 sample time mismatches.)
-    and writes a wf_miniseed document for each segment.  i.e. even a 
+    and writes a wf_miniseed document for each segment.  i.e. even a
     single channel, day file from a continuous data set may have multiple
-    segments defined because of gaps and time tears.  A huge complication of 
-    this reader is it attempts to handle all gap and overlap related issues 
-    and fix them when possible.   It cannot handle all situations, however, 
-    and takes the approach that if it cannot make a reasonable repair 
-    with some simple assumption it will kill the TimeSeries datum is is 
-    attempting to assemble and post an error message that can be used 
-    for post-mortum analysis of a workflow.  All the complexity of this 
+    segments defined because of gaps and time tears.  A huge complication of
+    this reader is it attempts to handle all gap and overlap related issues
+    and fix them when possible.   It cannot handle all situations, however,
+    and takes the approach that if it cannot make a reasonable repair
+    with some simple assumption it will kill the TimeSeries datum is is
+    attempting to assemble and post an error message that can be used
+    for post-mortum analysis of a workflow.  All the complexity of this
     processing is hidden under two layers of code this reader utilizes:
-        1.  The python function mspasspy.algorithms.window.merge 
-            is a wrapper that handles some translations from 
-        2.  The bottom layer, which, is a set of C++ code that does 99% of the 
+        1.  The python function mspasspy.algorithms.window.merge
+            is a wrapper that handles some translations from
+        2.  The bottom layer, which, is a set of C++ code that does 99% of the
             work.  The base layer was done in C++ for efficiency because,
             as noted above, use of this algorithm can be very time intensive
-            and requires efficiency.   The current implementation uses 
-            to C++ functions bound to python in the module 
-            mspasspy.ccore.algorithms.basic with symbols "splice_segments" 
-            and "repair_overlaps".  
-    
-    Users of this function will want to also read the docstring for 
-    mspasspy.algorithms.window.merge as it contains more implementation 
-    details on how the merge algorithm works and the assumptions it makes. 
-    
-    This reader is a high level reader to simplify reading time windows of 
-    data from a continuous data set.  The basic model is you specify a 
-    time window from which you want some subset or all the data 
+            and requires efficiency.   The current implementation uses
+            to C++ functions bound to python in the module
+            mspasspy.ccore.algorithms.basic with symbols "splice_segments"
+            and "repair_overlaps".
+
+    Users of this function will want to also read the docstring for
+    mspasspy.algorithms.window.merge as it contains more implementation
+    details on how the merge algorithm works and the assumptions it makes.
+
+    This reader is a high level reader to simplify reading time windows of
+    data from a continuous data set.  The basic model is you specify a
+    time window from which you want some subset or all the data
     with data recorded during that interval.  We further assume that
-    data has been indexed and is (by default and only thing sure to work) 
-    defined by set of wf_miniseed documents. You can specify a subset 
-    using the "base_query" argument (see below).  The algorithm then 
-    queries MongoDB to find all waveform segments that span all or part 
-    of the requested time interval.  It then reads and reorganizes the 
-    data into a list of TimeSeriesEnsemble objects.   There will be 
-    one such ensemble for each unique channel/location code found 
-    after applying the base query.   Hence, for example, if you used a 
-    query to only accept "B" channels with null location (loc attribute) 
+    data has been indexed and is (by default and only thing sure to work)
+    defined by set of wf_miniseed documents. You can specify a subset
+    using the "base_query" argument (see below).  The algorithm then
+    queries MongoDB to find all waveform segments that span all or part
+    of the requested time interval.  It then reads and reorganizes the
+    data into a list of TimeSeriesEnsemble objects.   There will be
+    one such ensemble for each unique channel/location code found
+    after applying the base query.   Hence, for example, if you used a
+    query to only accept "B" channels with null location (loc attribute)
     codes you could expect to get a list of at least 3 ensembles:   BHZ,
     BHE, and BHN (you might also get something like BH1 and BH2 - a detail).
-    All members of all ensembles will either have a complete vector within 
-    the specified range and be marked live or marked dead.  The default 
+    All members of all ensembles will either have a complete vector within
+    the specified range and be marked live or marked dead.  The default
     for gap handling is any gap or nonrepairable overlap will cause a dead
-    datum to be loaded with a elog entry defining the reason for the kill. 
-    If the boolean argument zero_gaps is set true (default is False) 
-    gaps will be zero filled and marked live.  The only evidence of that 
-    kind of problem in that mode is that a boolean Metadata attribute 
-    "has_gaps" will be set True (clean data will not have that attribute 
-    defined) and the gap time windows defined in a document with the 
-    key "gaps" with subdocuments defining the gap time intervals. 
-    
-    Note:  start times of all returned data will have a variation of + to - 
-      1 sample interval. If you need synchronous timing to a greater 
-      precision than one sample you will need to resample the outputs to 
+    datum to be loaded with a elog entry defining the reason for the kill.
+    If the boolean argument zero_gaps is set true (default is False)
+    gaps will be zero filled and marked live.  The only evidence of that
+    kind of problem in that mode is that a boolean Metadata attribute
+    "has_gaps" will be set True (clean data will not have that attribute
+    defined) and the gap time windows defined in a document with the
+    key "gaps" with subdocuments defining the gap time intervals.
+
+    Note:  start times of all returned data will have a variation of + to -
+      1 sample interval. If you need synchronous timing to a greater
+      precision than one sample you will need to resample the outputs to
       a fixed time comb.
-    
-    :param db: MongoDB database handle 
-    :type db:  Usually a MsPASS Database class but could be the superclass 
+
+    :param db: MongoDB database handle
+    :type db:  Usually a MsPASS Database class but could be the superclass
         MongoDB handle.
-        
+
     :param starttime:   starting time of time window of data to be extracted.
     :type starttime:  double epoch time
-    
+
     :param endtime: end time of time window of data to be extracted.
     :type starttime:  double epoch time
-    
-    :param base_query: A dictionary defining a higher level subset to 
-      limit what data are to be retrieved.  The most common would be 
-      limits on sample interval or channel naming.  Must be a valid 
+
+    :param base_query: A dictionary defining a higher level subset to
+      limit what data are to be retrieved.  The most common would be
+      limits on sample interval or channel naming.  Must be a valid
       pymongo find definition with a python dictionary.
     :type base_query:  python dictionary defining a valid MongoDB query.
-        
-    :param collection:  waveform collection to be read.  Default is 
-      wf_miniseed.  If you use any other collection name be aware the 
-      documents retrieved must have the keys "net", "sta", and "chan".  
-      ("loc" is treated as optional as in all of mspass) in the seed way AND 
-      have the time range of each datum defined by the keys "starttime" and 
+
+    :param collection:  waveform collection to be read.  Default is
+      wf_miniseed.  If you use any other collection name be aware the
+      documents retrieved must have the keys "net", "sta", and "chan".
+      ("loc" is treated as optional as in all of mspass) in the seed way AND
+      have the time range of each datum defined by the keys "starttime" and
       "endtime".  This function is know to work only with wf_miniseed
       documents created one of the mspass index_miniseed_* methods of Database.
     :type collection:  string
-    
-    :param fix_overlaps: see docstring for mspasspy.algorithms.window.merge 
+
+    :param fix_overlaps: see docstring for mspasspy.algorithms.window.merge
     :param zero_gaps: see docstring for mspasspy.algorithms.window.merge
-    :param save_zombies:  boolean controlling how data marked dead by 
-      the merge function are handled.   If set True the dead will be stuffed 
+    :param save_zombies:  boolean controlling how data marked dead by
+      the merge function are handled.   If set True the dead will be stuffed
       in the member vector of the returned ensembles.   If False (default)
       they will be silently discarded.
-    
+
     :return:  list of TimeSeriesEnsemble objects (see main description for
         how the data in these containers are grouped)
     """
@@ -324,13 +324,13 @@ def TimeIntervalReader(
 
 def _initialize_ensemble(doc, tstart, tend) -> TimeSeriesEnsemble:
     """
-    Helper for above to do the repititious task of creating a skeleton 
-    ensemble to be filled by atomic reads.  Creates a basic ensemble 
-    and then loads an ensemble header appropriate for the application of 
+    Helper for above to do the repititious task of creating a skeleton
+    ensemble to be filled by atomic reads.  Creates a basic ensemble
+    and then loads an ensemble header appropriate for the application of
     carving out a time window.
-    
-    :param doc: doc to use as pattern 
-    :param tstart: start time (epoch time) of time window 
+
+    :param doc: doc to use as pattern
+    :param tstart: start time (epoch time) of time window
     :param tend: end time (epoch time) of time window
     """
     ens = TimeSeriesEnsemble()
