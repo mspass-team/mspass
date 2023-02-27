03e83e3b60517e2810b3f7bcdacbd75ae7c2524e
Zhengtang Yang <ztyang@login1.stampede2.tacc.utexas.edu>
2023-02-27 08:13:41 -0600

    Some minor changes: check whether the scripts are executable before running delete some personal information migrate to tacc (users should set their configurations anyway)

--- scripts/template/job_script.sh
+++ scripts/template/job_script.sh
@@ -2,21 +2,32 @@
 # If your cluster uses slurm top needs to be some variant of this
 # Check documentation for additional arguments that may be required or desirable
 #SBATCH -J mspass           # Job name
-#SBATCH -o pavlis.o%j       # Name of stdout output file
-#SBATCH -N 2                # Total # of nodes - 3 for this example
+#SBATCH -o mspass.o%j       # Name of stdout output file
+#SBATCH -p skx-normal       # Queue (partition) name - system dependent
+#SBATCH -N 2                # Total # of nodes
 #SBATCH -n 2                # Total # of mpi tasks (normally the same as -N)
 #SBATCH -t 00:10:00         # Run time (hh:mm:ss)
+#SBATCH -A MsPASS           # Allocation name (req'd if you have more than 1)
 
 # This is a typical load line with module.   The equivalent functionality
 # locally may be different.  Functionality is like shell "dot" initialization
 # files
-module load singularity
-module load openmpi
+ml unload xalt
+ml tacc-singularity
 module list
 
 # See User' Manual for guidance on building this file
 # Actual file path is system/user dependent
-source /N/slate/pavlis/test_scripts/mspass_setup.sh
+if [[ -x "mspass_setup.sh"  ]]
+then
+    echo "the setup file is read "
+else
+    echo "the setup file is not executable"
+    chmod +x mspass_setup.sh
+fi
+
+source mspass_setup.sh
+
 # Note your jupyter notebook must be in this directory for this script
 cd $MSPASS_WORK_DIR
 
@@ -31,4 +42,13 @@ else
     done
 fi
 echo Running $notebook_file
+
+if [[ -x "$MSPASS_RUNSCRIPT"  ]]
+then
+    echo "$MSPASS_RUNSCRIPT is ready"
+else
+    echo "$MSPASS_RUNSCRIPT is not executable"
+    chmod +x $MSPASS_RUNSCRIPT
+fi
+
 $MSPASS_RUNSCRIPT $notebook_file
--- scripts/template/mspass_setup.sh
+++ scripts/template/mspass_setup.sh
@@ -3,24 +3,24 @@
 # See User's Manual for more guidance on setting these variables
 export MSPASS_HOME=~/mspass
 # full path to the singularity container
-export MSPASS_CONTAINER=${MSPASS_HOME}/containers/mspass_latest.sif
+export MSPASS_CONTAINER=$WORK2/mspass/images/mspass_latest
 # If needed list all file systems names that should be mounted when
-# the container boots.  Usually an explicit path is best to avoid 
+# the container boots.  Usually an explicit path is best to avoid
 # errors from aliasing a directory name
-export SINGULARITY_BIND=/N/slate/pavlis,/N/scratch/pavlis
+export SINGULARITY_BIND=~/others
 
 # if not set this defaults to the directory where the run script is called
-export MSPASS_WORK_DIR=/N/slate/pavlis/test_scripts
+export MSPASS_WORK_DIR=$WORK/temp
 # if not set this defaults to $MSPASS_WORK_DIR/db
-export MSPASS_DB_DIR=/N/scratch/pavlis/usarray/db
+#export MSPASS_DB_DIR=$WORK/temp/db
 # if not set this defaults to $MSPASS_WORK_DIR/logs
-export MSPASS_LOG_DIR=/N/scratch/pavlis/usarray/logs
+#export MSPASS_LOG_DIR=$WORK/temp/logs
 # if not set this defaults to $MSPASS_WORK_DIR/work
-export MSPASS_WORKER_DIR=/N/scratch/pavlis/usarray/work
+#export MSPASS_WORKER_DIR=$WORK/temp/work
 
 
-export HOSTNAME_BASE="carbonate.uits.iu.edu"
+export HOSTNAME_BASE="stampede2.tacc.utexas.edu"
 
 if [ -z $MSPASS_RUNSCRIPT ] ; then
-    export MSPASS_RUNSCRIPT=/N/slate/pavlis/test_scripts/run_mspass.sh
+    export MSPASS_RUNSCRIPT=./run_mspass.sh
 fi
