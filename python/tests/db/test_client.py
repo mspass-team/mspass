02ef8256a709f356af436fa1e34198455fe7f0e0
wangyinz <wangyinz@users.noreply.github.com>
2023-06-19 07:13:23 +0000

    :art: Format Python code with psf/black

--- python/tests/db/test_client.py
+++ python/tests/db/test_client.py
@@ -23,10 +23,8 @@ class TestDBClient:
         ):
             self.c2.get_default_database()
         db1 = self.c1.get_default_database(schema="mspass_lite.yaml")
-        with pytest.raises(
-            KeyError, match="site"
-        ):
-            db1.database_schema._attr_dict['site']
+        with pytest.raises(KeyError, match="site"):
+            db1.database_schema._attr_dict["site"]
 
     def test_get_database(self):
         assert self.c1.get_database().name == "my_database"
@@ -36,7 +34,5 @@ class TestDBClient:
         ):
             self.c2.get_database()
         db1 = self.c1.get_database(schema="mspass_lite.yaml")
-        with pytest.raises(
-            KeyError, match="site"
-        ):
-            db1.database_schema._attr_dict['site']
+        with pytest.raises(KeyError, match="site"):
+            db1.database_schema._attr_dict["site"]
