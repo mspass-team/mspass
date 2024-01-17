dac6e7f1729a67e87eaa44658263e18e705488aa
Gary Pavlis <pavlis@indiana.edu>
2024-01-17 09:21:08 -0500

    Fix bug in handling starttime and endtime
    Previous version was failing to set starttime and endtime on saves.  Also data loaded and cut had incorrect starttime and endtimes saved because they werne't being replaced.

--- python/mspasspy/db/database.py
+++ python/mspasspy/db/database.py
@@ -6508,6 +6508,9 @@ class Database(pymongo.database.Database):
         # add tag - intentionally not set in mspass_object returned
         if data_tag:
             insertion_dict["data_tag"] = data_tag
+        # Always set starttime and endtime 
+        insertion_dict["starttime"] = mspass_object.t0
+        insertion_dict("endtime"] = mspass_object.endtime()
         else:
             # We need to clear data tag if was previously defined in
             # this case or a the old tag will be saved with this datum
