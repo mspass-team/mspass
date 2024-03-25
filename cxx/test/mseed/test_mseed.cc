bea26aec7585b7e10dae6c9db42914735e2b6619
Gary Pavlis <pavlis@indiana.edu>
2024-03-25 06:52:34 -0400

    Add assert statements for testing against new data file

--- cxx/test/mseed/test_mseed.cc
+++ cxx/test/mseed/test_mseed.cc
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include "mspass/io/mseed_index.h"
 #include "mspass/seismic/TimeSeries.h"
 #include "mspass/utility/ErrorLogger.h"
@@ -29,6 +30,29 @@ int main(int argc, char **argv)
       << " " << x->starttime
       << " " << x->endtime<<endl;
 	cout << "Size of error log="<<elog.size()<<endl;
+  /* these tests are keyed to one and only one test file.  
+     They will need to be changed if the file is changed
+     to add tests for problem data - may be needed in 
+     the future */
+  assert(ind.size()==5);
+  assert(ind[0].sta=="E2000");
+  assert(ind[0].chan=="VHE");
+  assert(ind[0].foff==0);
+  assert(ind[0].nbytes==4096);
+  assert(ind[0].npts==70);
+  assert(fabs(ind[0].samprate-0.1)<0.0001);
+  assert(ind[1].sta=="E2000");
+  assert(ind[1].foff==4096);
+  assert(ind[1].npts==2434403);
+  assert(ind[2].sta=="A2000");
+  assert(ind[2].chan=="UHE");
+  assert(ind[2].foff==1921024);
+  assert(ind[2].nbytes==4096);
+  assert(ind[2].npts==15);
+  assert(fabs(ind[2].samprate-0.01)<0.0001);
+  assert(ind[3].sta=="A2000");
+  assert(ind[3].foff==1925120);
+  assert(ind[3].npts==47205);
 }
 
 
