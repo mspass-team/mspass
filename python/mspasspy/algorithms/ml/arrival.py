97e6f354074dd6cd12348835582c7ed7fd7b93fa
JiaomaWHU <jinxin.ma9801@gmail.com>
2024-04-11 23:08:18 -0700

    use timewindow object and stored arrival time in the relative format

--- python/mspasspy/algorithms/ml/arrival.py
+++ python/mspasspy/algorithms/ml/arrival.py
@@ -4,28 +4,38 @@ import seisbench.models as sbm
 from obspy.taup import TauPyModel
 from obspy.geodetics import gps2dist_azimuth, kilometers2degrees
 from mspasspy.ccore.seismic import TimeSeries, TimeSeriesEnsemble
+from mspasspy.ccore.algorithms.basic import TimeWindow, WindowData
 from seisbench.models.base import WaveformModel
 
 
 def annotate_arrival_time(
     timeseries: TimeSeries,
     threshold=0.2,
-    window_start_offset_sec=0,
-    window_length_sec=None,
+    time_window: TimeWindow = None,
     model: WaveformModel = None,
     model_args: dict = None,
 ):
     """
-    Predict the arrival time of the seismic phase using the provided seisbench WaveformModel.
-    The arrival time will added to the input TimeSeries object and can be accessed using the key 'p_time'.
+    Predict the arrival time of the P wave using the provided seisbench WaveformModel.
+    The arrival time will saved as metadata in the input TimeSeries object and can be accessed using \
+        the key 'p_wave_picks'. Time is stored in the relative format using the start time of the TimeSeries.
 
     :param timeseries: The time series data to predict the arrival time.
+    :param threshold: The probability threshold (0-1) to filter p-wave picks. Default value is 0.2.
+    :param time_window: The relative time window to filter the predicted arrival time. If not provided, the whole time series will be used.
     :param model: The model used to predict the arrival time.
     :param model_args: arguments to initialize a new model if not provided
     :type timeseries: mspasspy.ccore.seismic.TimeSeries
+    :type threshold: float
+    :type time_window: mspasspy.ccore.algorithms.basic.TimeWindow
     :type model: seisbench.models.base.WaveformModel
     :type model_args: dict
     """
+
+    # Check the input arguments
+    if not 0 <= threshold <= 1:
+        raise ValueError("Threshold must be between 0 and 1")
+
     # load pretrained model based on the args if not provided
     if model == None:
         # 'stead' model was trained on STEAD for 100 epochs with a learning rate of 0.01.
@@ -34,15 +44,16 @@ def annotate_arrival_time(
         pretrained_model = "stead" if model_args["name"] == None else model_args["name"]
         model = sbm.PhaseNet.from_pretrained(pretrained_model)
 
-    # convert time series to stream
+    # apply the window if provided and convert time series to stream
+    windowed_timeseries = WindowData(timeseries, time_window.start, time_window.end) if time_window else timeseries
     ts_ensemble = TimeSeriesEnsemble()
-    ts_ensemble.member.append(timeseries)
+    ts_ensemble.member.append(windowed_timeseries)
     stream = ts_ensemble.toStream()
 
     # prediction result is the probability for picks over time
     pred_st = model.annotate(stream)
 
-    # Step 1: Access the data
+    # Step 1: Access the probability data
     for tr in pred_st:
         if trace.stats.channel == "PhaseNet_P":
             trace = tr
@@ -55,27 +66,14 @@ def annotate_arrival_time(
     # Step 3: Calculate the corresponding time
     # Trace start time
     start_time = trace.stats.starttime
-    window_start = trace.stats.starttime + window_start_offset_sec
-    window_end = window_start + window_length_sec if window_length_sec else trace.stats.endtime
-    if not window_start < window_end:
-        raise ValueError("Invalid time window")
-
     # Sampling rate (samples per second)
     sampling_rate = trace.stats.sampling_rate
-    # Calculate time offset in seconds
+    # Calculate time offset and arrival time of picks in seconds
     time_offsets = indices / sampling_rate
-    # Calculate the absolute time of the highest probability
-    arrival_times = start_time + time_offsets
+    p_wave_picks = start_time + time_offsets
 
-    # Step 4: Filter the indices and arrival times based on the time window
-    filtered_indices = []
-    filtered_arrival_times = []
-    for index, arrival_time in zip(indices, arrival_times):
-        if window_start <= arrival_time <= window_end:
-            filtered_indices.append(index)
-            filtered_arrival_times.append(arrival_time)
-
-    # Step 5: Convert arrival times to a more convenient format if necessary
-    filtered_arrival_time_strs = [time.strftime("%Y-%m-%d %H:%M:%S.%f") for time in filtered_arrival_times]
-
-    timeseries["p_arrival_picks_time"] = filtered_arrival_time_strs
+    # Step 4: Save the arrival time in the relative format
+    # Adjust the arrival time if a time window is provided
+    if time_window:
+        p_wave_picks += time_window.start
+    timeseries["p_wave_picks"] = p_wave_picks
