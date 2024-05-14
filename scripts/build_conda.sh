8624f79af234f59c8d36cd8090b8713637e132ad
wangyinz <yinzhi.wang.cug@gmail.com>
2024-05-14 16:55:31 -0500

    try removing files from source

--- scripts/build_conda.sh
+++ scripts/build_conda.sh
@@ -3,10 +3,10 @@
 # Install the package
 ${PYTHON} -m pip install . -vv
 
-rm -r $SRC_DIR/cxx/test
-rm -r $SRC_DIR/python/tests
-rm -r $SRC_DIR/docs
-rm -r $SRC_DIR/scripts
-rm -r $SRC_DIR/.github
-rm $SRC_DIR/Dockerfile*
-rm $SRC_DIR/docker-*
+rm -r cxx/test
+rm -r python/tests
+rm -r docs
+rm -r scripts
+rm -r .github
+rm Dockerfile*
+rm docker-*
