08df1f93d14f8c6527b6201dc5e401ec38f7681d
Gary Pavlis <pavlis@indiana.edu>
2024-11-21 08:00:13 -0500

    Add docstrings and additional comments to test file for Janitor

--- python/tests/util/test_Janitor.py
+++ python/tests/util/test_Janitor.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+This file contains test functions to be run under pytest.
+This set of functions test the Janitor class and it methods.
+For onsistency perhaps this should be a pytest class but the
+Janitor class is simple enough I (glp) didn't think that
+complexity (to me anyway) was necessary.
 Created on Thu Nov 14 14:33:17 2024
 
 @author: pavlis
 """
 from mspasspy.util.Janitor import Janitor
-#from Janitor import Janitor
+
 from helper import (
     get_live_seismogram,
     get_live_timeseries,
@@ -23,6 +28,16 @@ from mspasspy.ccore.utility import MsPASSError
 import pytest
 
 def test_Janitor_constructor():
+    """
+    This function does a basic tests of the constructor 
+    for Janitor that is based on a yaml file.   The code used by 
+    the constructor is a variant of the mspass schema constuctor.  
+    I am not certain I've tested all variants the constructor 
+    could handle, the worst of which is formatting errors in the 
+    yaml file.  I don't want to corrupt the test data with an intentionally 
+    bad yaml file.  
+    """
+
     # test default yaml creating from yaml file
     j1 = Janitor()
     assert len(j1.TimeSeries_keepers) > 0
@@ -42,18 +57,23 @@ def test_Janitor_constructor():
         
 def generic_methods_tester_atomic(t):
     """
-    Generic function used to test methods on all supported data types. 
+    Generic function used to test methods on all atomic data types. 
     input t must be a type object that resolves to one of:
-    TimeSeries or Seismogram
+    TimeSeries or Seismogram.   There is a variant below because 
+    we have to handle a couple methods differently with ensembles
+    than atomic data.
+    
+    This function is not run directly by pytest because it 
+    intentionally does not have a name beginning with "test".  
+    It is called from the function "test_Janitor_methods" with 
+    variable type names. 
     """
     janitor = Janitor()
     # use isinstance to get a working object to run test upon
     if t == TimeSeries:
         d = get_live_timeseries()
-        d0 = TimeSeries(d)
     elif t == Seismogram:
         d = get_live_seismogram()
-        d0 = Seismogram(d)
     else:
         raise ValueError("illegal value for arg0={}".t)
     k0 = d.keys()
@@ -107,9 +127,19 @@ def generic_methods_tester_atomic(t):
     
 def generic_methods_tester_ensembles(t):
     """
-    Generic function used to test methods on all supported data types. 
+    Generic function used to test methods on all ensemble data types. 
     input t must be a type object that resolves to one of:
-    TimeSeries or Seismogram
+    TimeSeriesEnemble or SeismogramEnsemble. 
+    
+    This is a variant of the atomic version.  The primary difference is
+    it has to test the behavior of methods that need to distinguish 
+    ensemble container Metadata versus an implied loop over all members 
+    (the default behavior).   
+    
+    This function is not run directly by pytest because it 
+    intentionally does not have a name beginning with "test".  
+    It is called from the function "test_Janitor_methods" with 
+    variable type names. 
     """
     # first test behavior on ensemble Metadata
     janitor = Janitor(process_ensemble_members=False)
@@ -178,14 +208,28 @@ def generic_methods_tester_ensembles(t):
     assert "Ptime" in d
     # now repeat but applying to members instead of enemble container.
 def test_janitor_methods():
+    """
+    This is the function run by pytest to test the methods of 
+    Janitor.  It uses two functions defined above with a type name 
+    to define the type of data to test.  Janitor defines different 
+    attributes as keepers for differnt types which makes the different 
+    type tests necessary. 
+    """
     generic_methods_tester_atomic(TimeSeries)
     generic_methods_tester_atomic(Seismogram)
     generic_methods_tester_ensembles(TimeSeriesEnsemble)
     generic_methods_tester_ensembles(SeismogramEnsemble)
 def test_janitor_error_handlers():
+    """
+    Test the error handler for Janitor.  At present the only thing 
+    that will throw an exception is the constructor.   
+    
+    TODO:   the tests here are incomplete for possible issues in 
+    problems with the environment variale MSPASS_HOME and format 
+    errors in the yaml file.  I do not know how to test those handlers 
+    without creating junk files that would only confuse the code base. 
+    """
     with pytest.raises(MsPASSError,match="Cannnot open keepers_file"):
         j = Janitor(keepers_file="bad_file_name")
 
-#test_Janitor_constructor()
-#test_janitor_methods()
-#test_janitor_error_handlers()
+
