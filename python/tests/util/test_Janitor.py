2f2a2d1375ab8d5e00f0d2f6e9647dd737a6f996
Gary Pavlis <pavlis@indiana.edu>
2024-11-29 07:36:58 -0500

    Format with black

--- python/tests/util/test_Janitor.py
+++ python/tests/util/test_Janitor.py
@@ -27,46 +27,48 @@ from mspasspy.ccore.seismic import (
 from mspasspy.ccore.utility import MsPASSError
 import pytest
 
+
 def test_Janitor_constructor():
     """
-    This function does a basic tests of the constructor 
-    for Janitor that is based on a yaml file.   The code used by 
-    the constructor is a variant of the mspass schema constuctor.  
-    I am not certain I've tested all variants the constructor 
-    could handle, the worst of which is formatting errors in the 
-    yaml file.  I don't want to corrupt the test data with an intentionally 
-    bad yaml file.  
+    This function does a basic tests of the constructor
+    for Janitor that is based on a yaml file.   The code used by
+    the constructor is a variant of the mspass schema constuctor.
+    I am not certain I've tested all variants the constructor
+    could handle, the worst of which is formatting errors in the
+    yaml file.  I don't want to corrupt the test data with an intentionally
+    bad yaml file.
     """
 
     # test default yaml creating from yaml file
     j1 = Janitor()
     assert len(j1.TimeSeries_keepers) > 0
     assert len(j1.Seismogram_keepers) > 0
-    # test override kwargs 
-    keylist = ["foo","bar","npts"]
+    # test override kwargs
+    keylist = ["foo", "bar", "npts"]
     j2 = Janitor(TimeSeries_keepers=keylist)
     kl = j2.TimeSeries_keepers
-    assert len(kl)==3
-    for k in keylist: 
+    assert len(kl) == 3
+    for k in keylist:
         assert k in kl
     j2 = Janitor(Seismogram_keepers=keylist)
     kl = j2.Seismogram_keepers
-    assert len(kl)==3
-    for k in keylist: 
+    assert len(kl) == 3
+    for k in keylist:
         assert k in kl
-        
+
+
 def generic_methods_tester_atomic(t):
     """
-    Generic function used to test methods on all atomic data types. 
+    Generic function used to test methods on all atomic data types.
     input t must be a type object that resolves to one of:
-    TimeSeries or Seismogram.   There is a variant below because 
+    TimeSeries or Seismogram.   There is a variant below because
     we have to handle a couple methods differently with ensembles
     than atomic data.
-    
-    This function is not run directly by pytest because it 
-    intentionally does not have a name beginning with "test".  
-    It is called from the function "test_Janitor_methods" with 
-    variable type names. 
+
+    This function is not run directly by pytest because it
+    intentionally does not have a name beginning with "test".
+    It is called from the function "test_Janitor_methods" with
+    variable type names.
     """
     janitor = Janitor()
     # use isinstance to get a working object to run test upon
@@ -89,7 +91,7 @@ def generic_methods_tester_atomic(t):
     assert len(kcleaned) == len(k0)
     for k in d.keys():
         assert k in k0
-        
+
     # similar tests for collect_trash
     d["foo"] = "bar"
     trash = janitor.collect_trash(d)
@@ -98,48 +100,48 @@ def generic_methods_tester_atomic(t):
     # collect_trash should also be equivalent to clean of d
     for k in d.keys():
         assert k in k0
-    
-    # test bag trash method which puts the trash in a dictionary 
+
+    # test bag trash method which puts the trash in a dictionary
     # with "trash" as the key to the data removed from d
     d["foo"] = "bar"
     d = janitor.bag_trash(d)
     assert "trash" in d
     x = d["trash"]
-    assert isinstance(x,dict)
+    assert isinstance(x, dict)
     assert x["foo"] == "bar"
-    
+
     # test add2keepers
     # here we need a fresh new timeseries in d
     janitor.add2keepers("Ptime")
-    if t==TimeSeries:
+    if t == TimeSeries:
         d = get_live_timeseries()
-    elif t==Seismogram:
+    elif t == Seismogram:
         d = get_live_seismogram()
-    d['Ptime'] = 10.0
+    d["Ptime"] = 10.0
     d["foo"] = "bar"
     d = janitor.bag_trash(d)
     assert d["Ptime"] == 10.0
     assert "trash" in d
-    x=d["trash"]
+    x = d["trash"]
     assert len(x) == 1
     assert x["foo"] == "bar"
-        
-    
+
+
 def generic_methods_tester_ensembles(t):
     """
-    Generic function used to test methods on all ensemble data types. 
+    Generic function used to test methods on all ensemble data types.
     input t must be a type object that resolves to one of:
-    TimeSeriesEnemble or SeismogramEnsemble. 
-    
+    TimeSeriesEnemble or SeismogramEnsemble.
+
     This is a variant of the atomic version.  The primary difference is
-    it has to test the behavior of methods that need to distinguish 
-    ensemble container Metadata versus an implied loop over all members 
-    (the default behavior).   
-    
-    This function is not run directly by pytest because it 
-    intentionally does not have a name beginning with "test".  
-    It is called from the function "test_Janitor_methods" with 
-    variable type names. 
+    it has to test the behavior of methods that need to distinguish
+    ensemble container Metadata versus an implied loop over all members
+    (the default behavior).
+
+    This function is not run directly by pytest because it
+    intentionally does not have a name beginning with "test".
+    It is called from the function "test_Janitor_methods" with
+    variable type names.
     """
     # first test behavior on ensemble Metadata
     janitor = Janitor(process_ensemble_members=False)
@@ -165,7 +167,7 @@ def generic_methods_tester_ensembles(t):
     assert len(kcleaned) == len(k0)
     for k in d.keys():
         assert k in k0
-        
+
     # similar tests for collect_trash
     d["foo"] = "bar"
     trash = janitor.collect_trash(d)
@@ -174,62 +176,64 @@ def generic_methods_tester_ensembles(t):
     # collect_trash should also be equivalent to clean of d
     for k in d.keys():
         assert k in k0
-    
-    # test bag trash method which puts the trash in a dictionary 
+
+    # test bag trash method which puts the trash in a dictionary
     # with "trash" as the key to the data removed from d
     d["foo"] = "bar"
     d = janitor.bag_trash(d)
     assert "ensemble_trash" in d
     x = d["ensemble_trash"]
-    assert isinstance(x,dict)
+    assert isinstance(x, dict)
     assert x["foo"] == "bar"
-    
+
     # test add2keepers
     # here we need a fresh new timeseries in d
-    janitor.add2keepers("Ptime","ensemble")
-    if t==TimeSeriesEnsemble:
+    janitor.add2keepers("Ptime", "ensemble")
+    if t == TimeSeriesEnsemble:
         d = TimeSeriesEnsemble(d0)
-    elif t==SeismogramEnsemble:
+    elif t == SeismogramEnsemble:
         d = SeismogramEnsemble(d0)
-    d['Ptime'] = 10.0
+    d["Ptime"] = 10.0
     d["foo"] = "bar"
     d = janitor.bag_trash(d)
     assert d["Ptime"] == 10.0
     assert "ensemble_trash" in d
-    x=d["ensemble_trash"]
+    x = d["ensemble_trash"]
     assert len(x) == 1
     assert x["foo"] == "bar"
     # add a value to the keepers for ensemble - default is empty
-    janitor.add2keepers("source_id","ensemble")
-    d['source_id'] = 'a source id place holder'
+    janitor.add2keepers("source_id", "ensemble")
+    d["source_id"] = "a source id place holder"
     d = janitor.clean(d)
-    assert len(d.keys())==2
+    assert len(d.keys()) == 2
     assert "source_id" in d
     assert "Ptime" in d
     # now repeat but applying to members instead of enemble container.
+
+
 def test_janitor_methods():
     """
-    This is the function run by pytest to test the methods of 
-    Janitor.  It uses two functions defined above with a type name 
-    to define the type of data to test.  Janitor defines different 
-    attributes as keepers for differnt types which makes the different 
-    type tests necessary. 
+    This is the function run by pytest to test the methods of
+    Janitor.  It uses two functions defined above with a type name
+    to define the type of data to test.  Janitor defines different
+    attributes as keepers for differnt types which makes the different
+    type tests necessary.
     """
     generic_methods_tester_atomic(TimeSeries)
     generic_methods_tester_atomic(Seismogram)
     generic_methods_tester_ensembles(TimeSeriesEnsemble)
     generic_methods_tester_ensembles(SeismogramEnsemble)
+
+
 def test_janitor_error_handlers():
     """
-    Test the error handler for Janitor.  At present the only thing 
-    that will throw an exception is the constructor.   
-    
-    TODO:   the tests here are incomplete for possible issues in 
-    problems with the environment variale MSPASS_HOME and format 
-    errors in the yaml file.  I do not know how to test those handlers 
-    without creating junk files that would only confuse the code base. 
+    Test the error handler for Janitor.  At present the only thing
+    that will throw an exception is the constructor.
+
+    TODO:   the tests here are incomplete for possible issues in
+    problems with the environment variale MSPASS_HOME and format
+    errors in the yaml file.  I do not know how to test those handlers
+    without creating junk files that would only confuse the code base.
     """
-    with pytest.raises(MsPASSError,match="Cannnot open keepers_file"):
+    with pytest.raises(MsPASSError, match="Cannnot open keepers_file"):
         j = Janitor(keepers_file="bad_file_name")
-
-
