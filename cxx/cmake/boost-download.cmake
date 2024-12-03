d87e1d4a389bc99fc4a887b50ca969c9020140e1
Gary Pavlis <pavlis@indiana.edu>
2024-12-03 18:12:01 -0500

    Fixes to get boost to compile on macos - may be a problem

--- cxx/cmake/boost-download.cmake
+++ cxx/cmake/boost-download.cmake
@@ -8,10 +8,10 @@ ExternalProject_Add(
   boost
   SOURCE_DIR "@BOOST_DOWNLOAD_ROOT@/boost-src"
   URL
-    https://boostorg.jfrog.io/artifactory/main/release/1.71.0/source/boost_1_71_0.tar.gz
+    https://boostorg.jfrog.io/artifactory/main/release/1.86.0/source/boost_1_86_0.tar.gz
   URL_HASH
-    SHA256=96b34f7468f26a141f6020efb813f1a2f3dfb9797ecf76a7d7cbd843cc95f5bd
-  CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR}
+    SHA256=2575e74ffc3ef1cd0babac2c1ee8bdb5782a0ee672b1912da40e5b4b591ca01f
+  CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR} --with-toolset=gcc
   BUILD_COMMAND ./b2 cxxflags=-fPIC --with-serialization -j 8
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND ./b2 install
