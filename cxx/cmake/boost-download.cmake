83909f2a0cf497d07783ea58499176f33e337d17
Gary Pavlis <pavlis@indiana.edu>
2025-01-07 10:32:30 -0500

    Fix boost download url problem (#594)
    * Fix stale boost download url inconsistent with hash value
    
    * Update action/cache to v4
    
    ---------
    
    Co-authored-by: Ian Wang <22849821+wangyinz@users.noreply.github.com>

--- cxx/cmake/boost-download.cmake
+++ cxx/cmake/boost-download.cmake
@@ -8,9 +8,10 @@ ExternalProject_Add(
   boost
   SOURCE_DIR "@BOOST_DOWNLOAD_ROOT@/boost-src"
   URL
-    https://boostorg.jfrog.io/artifactory/main/release/1.86.0/source/boost_1_86_0.tar.gz
+    https://archives.boost.io/release/1.86.0/source/boost_1_86_0.tar.gz
   URL_HASH
     SHA256=2575e74ffc3ef1cd0babac2c1ee8bdb5782a0ee672b1912da40e5b4b591ca01f
+
   CONFIGURE_COMMAND ./bootstrap.sh --prefix=${PROJECT_BINARY_DIR} --with-toolset=gcc
   BUILD_COMMAND ./b2 cxxflags=-fPIC --with-serialization -j 8
   BUILD_IN_SOURCE 1
