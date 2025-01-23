e19b12232a598a4b48893a87a433afb149f7eddd
Gary Pavlis <pavlis@indiana.edu>
2025-01-23 10:35:25 -0500

    Bug fixes to snr module and related C++ code (#598)
    * Fix bug in scaling that caused NaNs whith an all zero vector
    
    * Fix bug in not handling illegal valuues in bandwidth method
    
    * Fix bugs in handling low snr data found in more extensive real data testing
    
    * Clean format with black
    
    * Major additions to snr pytest functions
    
    * format with black

--- python/tests/algorithms/test_snr.py
+++ python/tests/algorithms/test_snr.py
@@ -1,5 +1,5 @@
-from mspasspy.ccore.algorithms.deconvolution import MTPowerSpectrumEngine
-from mspasspy.ccore.seismic import TimeSeries, TimeReferenceType
+from mspasspy.ccore.seismic import TimeSeries, TimeReferenceType, DoubleVector
+from mspasspy.ccore.utility import MsPASSError
 from mspasspy.ccore.algorithms.basic import TimeWindow
 from mspasspy.algorithms.snr import (
     snr,
@@ -10,11 +10,10 @@ from mspasspy.algorithms.snr import (
 )
 from mspasspy.db.client import DBClient
 from mspasspy.db.database import Database
+from bson import json_util
 import numpy as np
-from scipy import signal
-from bson import json_util, ObjectId
 import pickle
-
+import pytest
 
 ######################################################################
 # This block of code was used to create the test file for this
@@ -83,24 +82,19 @@ def verify_snr_outputs_match(so1, so2):
         assert np.isclose(so1[k], so2[k])
 
 
-def test_snr():
+def test_snr_functions():
     """
-    Test function for snr module.   This function is currently incomplete as it
-    needs to test a new implemenation of the EstimateBandwidth function
-    independent from the higher level functions tested here.   Most of that
-    function is tested by this one since some of the higher level functions use
-    the EstimateBandwidth function.
-
-    TODO:  the biggest gap her is that most of the error handlers are tested.
+    The snr module has a set of simple snr metrics tested by
+    this function.
     """
     with open("python/tests/data/snrtestdata", "rb") as pickle_file:
-        ts = pickle.load(pickle_file)
+        ts0 = pickle.load(pickle_file)
+    ts = TimeSeries(ts0)
     nwin = TimeWindow(-200.0, -50.0)
     # appropriate for perc and peak
     swin = TimeWindow(-10.0, 50.0)
     # used for rms and mad for this test
     swin2 = TimeWindow(-0.5, 10.0)
-
     snrrms = snr(ts, nwin, swin2, noise_metric="rms", signal_metric="rms")
     print("snr_rms=", snrrms)
     snrpeak_rms = snr(ts, nwin, swin, noise_metric="rms", signal_metric="peak")
@@ -118,6 +112,24 @@ def test_snr():
     assert np.isclose(snrmad_rms, 6.278840062165531)
     assert np.isclose(snrperc_rms, 11.237789966979753)
 
+
+def test_FD_snr_estimator():
+    """
+    Test function for snr module.   This function is currently incomplete as it
+    needs to test a new implemenation of the EstimateBandwidth function
+    independent from the higher level functions tested here.   Most of that
+    function is tested by this one since some of the higher level functions use
+    the EstimateBandwidth function.
+    """
+    with open("python/tests/data/snrtestdata", "rb") as pickle_file:
+        ts0 = pickle.load(pickle_file)
+        ts = TimeSeries(ts0)  # use this as working copy
+    nwin = TimeWindow(-200.0, -50.0)
+    # appropriate for perc and peak
+    swin = TimeWindow(-10.0, 50.0)
+    # used for rms and mad for this test
+    swin2 = TimeWindow(-0.5, 10.0)
+
     print("Output of FD_snr_estimator with default parameters")
     fd_snr_output = FD_snr_estimator(
         ts,
@@ -126,7 +138,7 @@ def test_snr():
         f0=1.0,
         band_cutoff_snr=2.0,
     )
-    print(json_util.dumps(fd_snr_output[0], indent=2))
+    # print(json_util.dumps(fd_snr_output[0], indent=2))
 
     elog = fd_snr_output[1]
     assert elog.size() == 0
@@ -207,9 +219,8 @@ def test_snr():
     tval = fd_snr_output[0]["snr_filtered_mad"]
     assert np.isclose(tval, 1.2763130717405682)
 
-    master = fd_snr_output[0]
-
     print("Repeat testing save_spectrum option")
+    ts = TimeSeries(ts0)
     # This one is for interactive testing - do no include in pytest
     fd_snr_output = FD_snr_estimator(
         ts, noise_window=nwin, signal_window=swin, save_spectra=True
@@ -224,6 +235,99 @@ def test_snr():
     assert sigspec.nf() == 6002
     assert nspec.nf() == 15002
 
+
+def test_FD_snr_estimator_error_handlers():
+    """
+    As the name implies this tests error handling features of FD_snr_estimator.
+    """
+    with open("python/tests/data/snrtestdata", "rb") as pickle_file:
+        ts0 = pickle.load(pickle_file)
+        ts = TimeSeries(ts0)  # use this as working copy
+    # test handling of a vector of all zero - earlier versions caused an NaN
+    # in this situation
+    d0 = np.zeros(ts.npts)
+    ts.data = DoubleVector(d0)
+    nwin = TimeWindow(-200.0, -50.0)
+    swin = TimeWindow(-10.0, 50.0)
+    fd_snr_output = FD_snr_estimator(
+        ts,
+        noise_window=nwin,
+        signal_window=swin,
+        f0=1.0,
+        band_cutoff_snr=2.0,
+    )
+    # should return an empty dict in 0 for this condition
+    # with a posted error message in 1
+    assert len(fd_snr_output[0]) == 0
+    assert fd_snr_output[1].size() > 0
+
+    # test a handlers that throw exceptions
+
+    with pytest.raises(MsPASSError, match="Received invalid data object"):
+        fd_snr_output = FD_snr_estimator(
+            "badarg0",
+            noise_window=nwin,
+            signal_window=swin,
+            f0=1.0,
+            band_cutoff_snr=2.0,
+        )
+    # tests a rare user error entering an invalid number of tapes
+    ts = TimeSeries(ts0)
+    with pytest.raises(MsPASSError, match="ntapers="):
+        fd_snr_output = FD_snr_estimator(
+            ts,
+            ntapers=50,
+            noise_window=nwin,
+            signal_window=swin,
+            f0=1.0,
+            band_cutoff_snr=2.0,
+        )
+    # Test handling of dead datum
+    ts = TimeSeries(ts0)
+    ts.kill()
+    fd_snr_output = FD_snr_estimator(
+        ts,
+        noise_window=nwin,
+        signal_window=swin,
+        f0=1.0,
+        band_cutoff_snr=2.0,
+    )
+    assert len(fd_snr_output[0]) == 0
+    assert fd_snr_output[1].size() > 0
+
+
+def test_arrival_functions():
+    """
+    This function tests the arrival_snr function and broadband_snr_QC
+    functions that are higher level functions using the other functions
+    in this module to compute a user selected set of metrics.  We do
+    them together to validate outputs against a master run using
+    FD_snr_estimator.
+    """
+    with open("python/tests/data/snrtestdata", "rb") as pickle_file:
+        ts0 = pickle.load(pickle_file)
+        ts = TimeSeries(ts0)  # use this as working copy
+
+    # this reruns FD_estimator as above to create the master
+    # pattern of snr metrics validated
+    nwin = TimeWindow(-200.0, -50.0)
+    swin = TimeWindow(-10.0, 50.0)
+    fd_snr_output = FD_snr_estimator(
+        ts,
+        noise_window=nwin,
+        signal_window=swin,
+        band_cutoff_snr=2.0,
+        optional_metrics=[
+            "snr_stats",
+            "filtered_envelope",
+            "filtered_L2",
+            "filtered_Linf",
+            "filtered_MAD",
+            "filtered_perc",
+        ],
+    )
+    master = fd_snr_output[0]
+
     print("Testing arrival_snr function with autoshift")
     # Now test arrival_snr.  That function is mainly a front end to
     # FD_snr_estimator to handle time shifting for an arrival window.
@@ -231,7 +335,7 @@ def test_snr():
     # We just change t0 but don't mess with time reference as it isn't
     # required here.  Beware that could change down the road and
     # break this test as that is an implementation detail
-    ts2 = TimeSeries(ts)
+    ts2 = TimeSeries(ts0)
     ts2.t0 = 100000.0 + ts.t0
     ts2["Ptime"] = 100000.0
     ts2.tref = TimeReferenceType.UTC
@@ -256,6 +360,7 @@ def test_snr():
     # Finally test the database function to save results of previous
     # function to an arrival collection.
     dbclient = DBClient("localhost")
+    dbclient.drop_database("test_snrQC")
     db = dbclient.get_database("test_snrQC")
     doc_to_save = asnr_out3["Parrival"]
     # Fake the id as if these data had been read from db.  The
@@ -284,3 +389,9 @@ def test_snr():
         update_id=idout,
     )
     assert idout2 == idout
+
+
+# test_snr_functions()
+# test_FD_snr_estimator()
+# test_arrival_functions()
+# test_FD_snr_estimator_error_handlers()
