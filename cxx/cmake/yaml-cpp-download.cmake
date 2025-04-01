7d5b089eabd9edb0406cdb17ef18322f2c510b8e
Ian Wang <yinzhi.wang.cug@gmail.com>
2025-04-01 12:42:51 -0500

    update cmake min version to 3.11

--- cxx/cmake/yaml-cpp-download.cmake
+++ cxx/cmake/yaml-cpp-download.cmake
@@ -1,4 +1,4 @@
-cmake_minimum_required(VERSION 3.4)
+cmake_minimum_required(VERSION 3.11)
 
 project(yaml-cpp-download NONE)
 
@@ -10,7 +10,7 @@ ExternalProject_Add(
   GIT_REPOSITORY
     https://github.com/jbeder/yaml-cpp.git
   GIT_TAG
-    yaml-cpp-0.6.3
+    0.8.0
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND cmake -DYAML_CPP_BUILD_TESTS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_INSTALL_PREFIX=${PROJECT_BINARY_DIR} .
   BUILD_COMMAND make -j 8
