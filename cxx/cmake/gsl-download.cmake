87d06ea997387a66a3056bff12d84278f112cf8c
Ian Wang <22849821+wangyinz@users.noreply.github.com>
2025-12-19 22:40:21 -0600

    Enhance GSL download options with more mirrors
    Added additional download mirrors for GSL.

--- cxx/cmake/gsl-download.cmake
+++ cxx/cmake/gsl-download.cmake
@@ -9,6 +9,11 @@ ExternalProject_Add(
   SOURCE_DIR "@GSL_DOWNLOAD_ROOT@/gsl-src"
   URL
     https://ftpmirror.gnu.org/gsl/gsl-latest.tar.gz
+    https://mirror.cs.odu.edu/gnu/gsl/gsl-latest.tar.gz
+    https://mirrors.ibiblio.org/gnu/gsl/gsl-latest.tar.gz
+    https://mirrors.ocf.berkeley.edu/gnu/gsl/gsl-latest.tar.gz
+    https://ftp.gnu.org/gnu/gsl/gsl-latest.tar.gz
+    http://mirrors.kernel.org/gnu/gsl/gsl-latest.tar.gz
   CONFIGURE_COMMAND ./configure --enable-shared=no --prefix=${PROJECT_BINARY_DIR} CFLAGS=-fPIC
   BUILD_COMMAND make -j 8
   BUILD_IN_SOURCE 1
