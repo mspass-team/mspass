704ced4f8f02d8a1491b0ad2e3b5b41a3477f064
wangyinz <yinzhi.wang.cug@gmail.com>
2026-03-21 12:34:25 -0500

    add debug print to conda build

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -3,6 +3,15 @@
 # Install the package
 ${PYTHON} -m pip install . --no-deps -vv
 
+# Debug block for intermittent pip resolver/index issues in CI.
+echo "===== MSPASS CONDA BUILD DEBUG START ====="
+${PYTHON} -V
+${PYTHON} -m pip --version
+${PYTHON} -m pip config debug || true
+env | grep -E '^(PIP_|HTTP_PROXY|HTTPS_PROXY|NO_PROXY|CONDA|SSL_CERT_FILE|REQUESTS_CA_BUNDLE)=' || true
+${PYTHON} -m pip index versions earthscope-sdk || true
+echo "===== MSPASS CONDA BUILD DEBUG END ====="
+
 # BEGIN_PIP_ONLY_DEPS
 ${PYTHON} -m pip install "earthscope-sdk"
 # END_PIP_ONLY_DEPS
