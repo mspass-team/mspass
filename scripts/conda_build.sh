e6de301452d51f185ee15bf077275c892320f96c
wangyinz <yinzhi.wang.cug@gmail.com>
2026-06-16 09:27:29 -0500

    Sync dependency metadata

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -4,5 +4,5 @@
 ${PYTHON} -m pip install . --no-deps -vv
 
 # BEGIN_PIP_ONLY_DEPS
-PIP_NO_INDEX=False ${PYTHON} -m pip install --no-deps "earthscope-sdk"
+PIP_NO_INDEX=False ${PYTHON} -m pip install --no-deps "earthscope-sdk>=1.5.0,<1.6.0"
 # END_PIP_ONLY_DEPS
