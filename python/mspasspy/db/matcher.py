aec89ab46b1db8c4e379ffc038cf54ac7981d843
Ian Wang <22849821+wangyinz@users.noreply.github.com>
2026-08-23 11:05:47 -0500

    Repair the public CSS arrival interval matcher (#975)
    * Fix CSS arrival matcher query contracts
    
    * Accept installed build in provenance guard
    
    * Close arrival matcher cursors on every path

--- python/mspasspy/db/matcher.py
+++ python/mspasspy/db/matcher.py
@@ -699,6 +699,7 @@ class css30_arrival_interval_matcher(NMF):
     ):
         """ """
         super().__init__(kill_on_failure, verbose)
+        self.startime_offset = startime_offset
         self.phasename = phasename
         self.phasename_key = phasename_key
         for x in attributes_to_load:
@@ -707,7 +708,9 @@ class css30_arrival_interval_matcher(NMF):
         for x in load_if_defined:
             self.load_if_defined.append(x)
         self.prepend_collection_name = prepend_collection_name
-        self.dbhandle = db[arrival_collection_name]
+        self.arrival_collection_name = arrival_collection_name
+        self.collection = arrival_collection_name
+        self.dbhandle = db[self.arrival_collection_name]
 
     def get_document(self, d):
         """
@@ -717,10 +720,11 @@ class css30_arrival_interval_matcher(NMF):
         :return: matching MongoDB document, or ``None`` when no match is found.
         :rtype: dict or None
         """
+
         stime = d.t0
         etime = d.endtime()
         query = {self.phasename_key: self.phasename}
-        query["time"] = {"$ge": stime, "$le": etime}
+        query["time"] = {"$gte": stime, "$lte": etime}
         n = self.dbhandle.count_documents(query)
         if n == 0:
             return None
@@ -728,39 +732,42 @@ class css30_arrival_interval_matcher(NMF):
             return self.dbhandle.find_one(query)
         else:
             cursor = self.dbhandle.find(query)
-            matchlist = []
-            # the key here perhaps should be set in constructor
-            # for now it is frozen as this constant
-            for doc in cursor:
-                # ignore any docs with the time attribute not set
-                if "time" in doc:
-                    dt = doc["time"] - self.time_offset
-                    matchlist.append([abs(dt), doc])
-            # handle these special cases
-            n_to_test = len(matchlist)
-            if n_to_test == 0:
-                raise MsPASSError(
-                    "css30_arrival_interval_matcher.get_document:  no arrival docs found with phasename set as"
-                    + self.phasename
-                    + " with a time attribute defined.  This should not happen and indicates a serious database inconsistence.  Aborting",
-                    ErrorSeverity.Fatal,
-                )
-            elif n_to_test == 1:
-                # weird syntax but this returns to doc of the one and only
-                # tuple getting through the above loop.  Execution of this
-                # block should be very very rare
-                return matchlist[0][1]
-            else:
-                dtmin = matchlist[0][0]
-                imin = 0
-                for i in range(len(matchlist) - 1):
-                    ii = i + 1
-                    dt = matchlist[ii][0]
-                    # not dt values are stored as abs differences
-                    if dt < dtmin:
-                        imin = ii
-                        dtmin = dt
-                return matchlist[imin][1]
+            try:
+                matchlist = []
+                # the key here perhaps should be set in constructor
+                # for now it is frozen as this constant
+                for doc in cursor:
+                    # ignore any docs with the time attribute not set
+                    if "time" in doc:
+                        dt = doc["time"] - (d.t0 + self.startime_offset)
+                        matchlist.append([abs(dt), doc])
+                # handle these special cases
+                n_to_test = len(matchlist)
+                if n_to_test == 0:
+                    raise MsPASSError(
+                        "css30_arrival_interval_matcher.get_document:  no arrival docs found with phasename set as"
+                        + self.phasename
+                        + " with a time attribute defined.  This should not happen and indicates a serious database inconsistence.  Aborting",
+                        ErrorSeverity.Fatal,
+                    )
+                elif n_to_test == 1:
+                    # weird syntax but this returns to doc of the one and only
+                    # tuple getting through the above loop.  Execution of this
+                    # block should be very very rare
+                    return matchlist[0][1]
+                else:
+                    dtmin = matchlist[0][0]
+                    imin = 0
+                    for i in range(len(matchlist) - 1):
+                        ii = i + 1
+                        dt = matchlist[ii][0]
+                        # not dt values are stored as abs differences
+                        if dt < dtmin:
+                            imin = ii
+                            dtmin = dt
+                    return matchlist[imin][1]
+            finally:
+                cursor.close()
 
     def normalize(self, d):
         """
@@ -800,7 +807,7 @@ class css30_arrival_interval_matcher(NMF):
                         # but it could create bloated elog collections
                         message = (
                             "No data for key="
-                            + self.mdkey
+                            + key
                             + " in document returned from collection="
                             + self.collection
                         )
@@ -822,8 +829,10 @@ class css30_arrival_interval_matcher(NMF):
                         d[mdkey] = doc[key]
                     elif self.verbose:
                         self.log_error(
+                            d,
                             "css30_arrival_interval_matcher",
                             "No data found with optional load key=" + key,
+                            False,
                             ErrorSeverity.Informational,
                         )
 
