bf0b7358a7c305f6dd35028980f50f491de00aee
wangyinz <22849821+wangyinz@users.noreply.github.com>
2026-08-23 17:24:42 +0000

    Deploying to gh-pages from  @ eed4fcf3de912ee71d94a4644a4f42dd8b944b2c 🚀

--- latest/_static/documentation_options.js
+++ latest/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '2.4.4.dev57+g348502193',
+    VERSION: '2.4.4.dev58+geed4fcf3d',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
